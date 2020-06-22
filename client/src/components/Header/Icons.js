3f6dfa5d54ff7cc23504b4b9e1ea40dba8c921a9
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-22 23:21:55 +0300

    Added User Component

--- client/src/components/Header/Icons.js
+++ client/src/components/Header/Icons.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./Icons.css";
 
+import User from "./User";
+
 class Icons extends React.Component{
     render(){
         return (
             <div id="header-icons">
-                <div className="icon user-icon flaticon-user">
-                    <span>Sign In | Join</span>
-                </div>
+                <User/>
                 <div className="icon cart-icon flaticon-shopping-cart">
                     <span className="badge">0</span>
                 
---
+++ client/src/components/Header/User.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import "./User.css";
+
+
+class User extends React.Component {
+    render(){
+        return (
+            <div className="icon user-icon flaticon-user">
+                <span>Sign In | Join</span>
+            </div>
+        )
+    }
+}
+
+
+export default User
