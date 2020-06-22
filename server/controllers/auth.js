883aadcc5574fee0644dd7856814e2f7f0bf4d8f
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-22 15:19:33 +0300

    wired up the auth routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,2 +1,106 @@
 const route = require("express").Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
+route.post(
+  "/api/login",
+  check("email").trim().isEmail().withMessage("Please enter a valid email"),
+  check("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("Your password must be a minimun of six characters"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(401).send(errors.array()[0].msg);
+      }
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).send({ message: "No user with that email" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).send({ message: "Passwords do not match" });
+      }
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      res.send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
+route.post(
+  "/api/register",
+  check("firstName")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage(
+      "Please enter your first name with a minimum of three characters"
+    ),
+  check("lastName")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage(
+      "Please enter your last name with a minimum of three characters"
+    ),
+  check("email").trim().isEmail().withMessage("Please enter a valid email"),
+  check("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("Please enter a password with a minimum of six characters"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(401).send(errors.array()[0].msg);
+      }
+      const {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName
+      } = req.body;
+      if (password !== confirmPassword) {
+        return res.status(401).send({ message: "Passwords do not match" });
+      }
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        return res
+          .status(401)
+          .send({ message: "A user with that email already exists" });
+      }
+      // **TODO**  CHECK IF EMAIL IS VALID VIA SENDGRID
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName
+      });
+      await user.save();
+      res.status(201).send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
+
+route.post("/api/logout", (req, res) => {
+  try {
+    req.session.destroy(err => {
+      if (err) {
+        return res.redirect("/");
+      }
+    });
+    res.send({ message: "Successfully logged out" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 module.exports = route;
