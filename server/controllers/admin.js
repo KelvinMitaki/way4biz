ebba6ad1df0925059703e4fba602b280f5a46cc3
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-22 12:43:35 +0300

    added routes for updating and deleting products

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -6,6 +6,32 @@ route.post("/api/product", async (req, res) => {
     const { name, price, quantity, subcategory } = req.body;
     const product = new Product({ name, price, quantity, subcategory });
     await product.save();
+    res.status(201).send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+route.delete("/api/product/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+    await Product.findByIdAndDelete(productId);
+    res.send({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+route.patch("/api/product/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const { name, price, quantity } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(productId, {
+      name,
+      price,
+      quantity
+    });
+    res.send(updatedProduct);
   } catch (error) {
     res.status(500).send(error);
   }
