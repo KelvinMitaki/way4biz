3223a028ba20947eaa5d5c1e7bb3f6eb753cbe4f
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-22 11:46:47 +0300

    connected to the databse

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+const mongooseConnect = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+  console.log("connected to the database");
+};
+mongooseConnect();
 app.listen(process.env.PORT, () =>
   console.log(`server started on port ${process.env.PORT}`)
 );
