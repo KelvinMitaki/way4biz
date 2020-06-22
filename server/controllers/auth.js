65118ca80a165a1adab29ebeede38c28627685cc
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-22 23:00:51 +0300

    added get and post route to reset password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 const route = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -100,6 +102,81 @@ route.post("/api/logout", auth, (req, res) => {
       }
     });
     res.send({ message: "Successfully logged out" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+route.post("/api/reset", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send({ message: "No user with that email found" });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30 minutes"
+    });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)(
+      // **TODO** from email address to be fixed
+      async () => {
+        try {
+          await sgMail.send({
+            to: email,
+            from: "kevinkhalifa911@gmail.com",
+            subject: "Password Resetting",
+            html: `<html lang="en">
+          <body>
+              <h5 style="font-family: Arial, Helvetica, sans-serif;">You requested for password reset</h5>
+              <p style="font-family: Arial, Helvetica, sans-serif;">Please Click
+                  <a href=${process.env.RESET_REDIRECT}/${token}>here</a> to reset your password
+              </p>
+          </body>
+          </html>`
+          });
+        } catch (error) {
+          console.log(error);
+          if (error.response) {
+            console.log(error.response.body);
+          }
+        }
+      }
+    )();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+route.get("/api/reset/:resetToken", async (req, res) => {
+  try {
+    const { resetToken } = req.params;
+    const decoded = jwt.verify(resetToken, process.env.JWT_SECRET);
+    if (!decoded._id) {
+      return res.status(401).send({ message: "Invalid Token" });
+    }
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      return res.status(401).send({ message: "No user found" });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+route.post("/api/reset/:resetToken", async (req, res) => {
+  try {
+    const { resetToken } = req.params;
+    const decoded = jwt.verify(resetToken, process.env.JWT_SECRET);
+    if (!decoded._id) {
+      return res.status(401).send({ message: "Invalid Token" });
+    }
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.findByIdAndUpdate(decoded._id, {
+      password: hashedPassword
+    });
+    res.send({ user, message: "Password updated successfully" });
   } catch (error) {
     res.status(500).send(error);
   }
