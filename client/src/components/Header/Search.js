2472e6e1d3d7aaa6034d392f2a8f786a9480ad0d
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-22 11:42:45 +0300

    Added Search component

---
+++ client/src/components/Header/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Search.css";
+
+
+class Search extends React.Component {
+    render(){
+        return (
+            <div id="header-search">
+
+            </div>
+        )
+    }
+}
+
+export default Search
