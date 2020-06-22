ce3c49ec743b36ffaed22c3371fafadbe3054167
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-22 11:28:19 +0300

    Added Header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css";
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Header
