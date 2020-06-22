4599f5df926278e3751cab6a835fb061b1bc5abd
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-22 14:24:56 +0300

    Added SIdebar Component

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -3,12 +3,16 @@ import React from "react";
 
 import "./Hero.css";
 
+import SideBar from "./SideBar";
+
 
 class Hero extends React.Component {
     render(){
         return (
-            <div id="hero">
-
+            <div id="hero" className="row">
+                <div className="container-fluid">
+                    <SideBar/>
+                </div>
             </div>
         )
     }
---
+++ client/src/components/Hero/SideBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import "./SideBar.css";
+
+
+class SideBar extends React.Component {
+    render(){
+        return (
+            <div id="sidebar" className="col-md-3">
+
+            </div>
+        )
+    }
+}
+
+
+export default SideBar
