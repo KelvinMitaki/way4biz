256933deff6c1a67083cacd6b5cdea8a4767c78b
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-22 14:10:22 +0300

    Added the Hero component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 class App extends React.Component{
   render(){
     return (
       <div className="App">
-      <Header/>
+        <Header/>
+        <Hero/>
       </div>
     );
   }
---
+++ client/src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import "./Hero.css";
+
+
+class Hero extends React.Component {
+    render(){
+        return (
+            <div id="hero">
+
+            </div>
+        )
+    }
+}
+
+
+export default Hero;
