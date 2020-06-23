87bcf9837b9bf6049b758c5f8701eb5545b49ba8
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-23 16:26:09 +0300

    added logic to fetch all orders

--- server/controllers/shop.js
+++ server/controllers/shop.js
@@ -1,6 +1,9 @@
-const Product = require("../models/Product");
-
 const route = require("express").Router();
+
+const Product = require("../models/Product");
+const auth = require("../middlewares/is-auth");
+const Order = require("../models/Order");
+
 route.get("/api/products", async (req, res) => {
   try {
     const products = await Product.find();
@@ -25,6 +28,15 @@ route.post("/api/products/search", async (req, res) => {
       name: { $regex: searchTerm, $options: "i" }
     });
     res.send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+route.get("/api/orders", auth, async (req, res) => {
+  try {
+    const orders = await Order.find({ buyer: req.session.user._id });
+    res.send(orders);
   } catch (error) {
     res.status(500).send(error);
   }
