7039b73a28b6afef9744ef4df3cbf435e6174a63
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-23 09:26:38 +0300

    Added MiniMenu component

--- client/src/components/MiniMenuWrapper/MiniMenuWrapper.js
+++ client/src/components/MiniMenuWrapper/MiniMenuWrapper.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import "./MiniMenuWrapper.css";
 
+import MiniMenu from "./MiniMenu";
+
 
 class MiniMenuWrapper extends React.Component {
     render(){
         return (
             <div id="mini-menu-wrapper">
-
+                <MiniMenu/>
             </div>
         )
     }
