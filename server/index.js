f8e35e03c297bde98af8a701c49260774047682b
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-23 18:00:13 +0300

    added node optimisation

--- server/index.js
+++ server/index.js
@@ -1,50 +1,63 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoStore = require("connect-mongodb-session")(session);
-const bodyParser = require("body-parser");
+const cluster = require("cluster");
+const os = require("os");
 
-const adminRoutes = require("./controllers/admin");
-const authRoutes = require("./controllers/auth");
-const shopRoutes = require("./controllers/shop");
+if (cluster.isMaster) {
+  // MASTER PROCESS
+  const n_cpus = os.cpus().length;
+  console.log(`forking ${n_cpus} processes`);
+  for (let i = 0; i < n_cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  // CHILD PROCESS
+  const express = require("express");
+  const mongoose = require("mongoose");
+  const session = require("express-session");
+  const MongoStore = require("connect-mongodb-session")(session);
+  const bodyParser = require("body-parser");
 
-const app = express();
+  const adminRoutes = require("./controllers/admin");
+  const authRoutes = require("./controllers/auth");
+  const shopRoutes = require("./controllers/shop");
 
-const sessionStore = new MongoStore({
-  uri: process.env.MONGO_URI,
-  collection: "sessions"
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  const app = express();
 
-app.use(
-  session({
-    name: process.env.SESSION_NAME,
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-    store: sessionStore,
-    cookie: {
-      sameSite: true,
-      maxAge: 1000 * 60 * 60,
-      secure: process.env.PRODUCTION
-    }
-  })
-);
-app.use(authRoutes);
-app.use(shopRoutes);
-app.use(adminRoutes);
-
-const mongooseConnect = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  const sessionStore = new MongoStore({
+    uri: process.env.MONGO_URI,
+    collection: "sessions"
   });
-  console.log("connected to the database");
-};
-mongooseConnect();
-app.listen(process.env.PORT, () =>
-  console.log(`server started on port ${process.env.PORT}`)
-);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(
+    session({
+      name: process.env.SESSION_NAME,
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      store: sessionStore,
+      cookie: {
+        sameSite: true,
+        maxAge: 1000 * 60 * 60,
+        secure: process.env.PRODUCTION
+      }
+    })
+  );
+  app.use(authRoutes);
+  app.use(shopRoutes);
+  app.use(adminRoutes);
+
+  const mongooseConnect = async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("connected to the database");
+  };
+  mongooseConnect();
+  app.listen(process.env.PORT, () =>
+    console.log(`server started on port ${process.env.PORT}`)
+  );
+}
