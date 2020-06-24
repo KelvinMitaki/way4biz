b55752a635d7b289e6f3df1c04f88e96d02d2532
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-24 12:29:57 +0300

    added timestamps to products

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,39 +1,42 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    stockQuantity: {
+      type: Number,
+      required: true
+    },
+    rating: {
+      type: Number
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    subcategory: {
+      type: String,
+      required: true
+    },
+    seller: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
   },
-  price: {
-    type: Number,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  stockQuantity: {
-    type: Number,
-    required: true
-  },
-  rating: {
-    type: Number
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  subcategory: {
-    type: String,
-    required: true
-  },
-  seller: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: true
-  }
-});
+  { timestamps: true }
+);
 const Product = mongoose.model("Product", ProductSchema);
 module.exports = Product;
