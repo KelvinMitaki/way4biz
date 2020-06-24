0fa12eb99ae5e4d2ced1da4e4e516ae3ab787a73
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-24 14:33:12 +0300

    changed the cart schema

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -2,17 +2,15 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import Market from "../Market/Market";
 
-
-class Home extends React.Component{
-    render(){
-        return (
-            <div>
-                <Hero/>
-                <Market/>
-            </div>
-        )
-    }
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hero />
+        <Market />
+      </div>
+    );
+  }
 }
 
-
-export default Home
+export default Home;
--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Types.ObjectId,
-    ref: "Product"
-  },
-  quantity: {
-    type: Number,
-    required: true
-  },
-  totalPrice: {
-    type: Number,
-    required: true
-  },
+  items: [
+    {
+      product: {
+        type: mongoose.Types.ObjectId,
+        ref: "Product"
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
   buyer: {
     type: mongoose.Types.ObjectId,
     ref: "User"
