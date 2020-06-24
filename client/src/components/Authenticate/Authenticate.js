9f373f9b5dc0c51d2f74cedc6ce2d095b267eea3
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-24 18:06:01 +0300

    Fixed some stuff

---
+++ client/src/components/Authenticate/Authenticate.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import "./Authenticate.css";
+
+
+class Authenticate extends React.Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default Authenticate
