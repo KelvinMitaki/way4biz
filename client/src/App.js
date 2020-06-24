621baada22c3e602fcba3d04cdb8364fda408529
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-24 18:57:35 +0300

    wired up redux form

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import {Route,Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import MiniMenuWrapper from "./components/MiniMenuWrapper/MiniMenuWrapper";
-import Product from "./components/Product/Product"
-import Home from './components/Pages/Home';
-import Cart from './components/Pages/Cart';
+import Product from "./components/Product/Product";
+import Home from "./components/Pages/Home";
+import Cart from "./components/Pages/Cart";
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Home} />
           <Route path="/product" exact component={Product} />
-          <Route path="/cart" exact component={Cart}/>
+          <Route path="/cart" exact component={Cart} />
         </Switch>
-        <Footer/>
-        <MiniMenuWrapper/>
+        <Footer />
+        <MiniMenuWrapper />
       </div>
     );
   }
 }
 
-export default App
+export default App;
