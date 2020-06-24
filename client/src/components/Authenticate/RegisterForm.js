301abc4674d926c9a1b06f4540961bc83c56b6c4
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-24 21:26:03 +0300

    added validation to the register page

--- client/src/components/Authenticate/RegisterForm.js
+++ client/src/components/Authenticate/RegisterForm.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./RegisterForm.css";
 import { reduxForm, Field } from "redux-form";
 import LoginField from "./Field";
+import validator from "validator";
 
 class RegisterForm extends React.Component {
   render() {
@@ -49,5 +50,36 @@ class RegisterForm extends React.Component {
     );
   }
 }
+const validate = formValues => {
+  const errors = {};
+  if (
+    !formValues.firstName ||
+    (formValues.firstName && formValues.firstName.trim().length < 2)
+  ) {
+    errors.firstName = "Please enter a valid first name";
+  }
+  if (
+    !formValues.lastName ||
+    (formValues.lastName && formValues.lastName.trim().length < 2)
+  ) {
+    errors.lastName = "Please enter a valid last name";
+  }
+  if (
+    !formValues.email ||
+    (formValues.email && validator.isEmail(formValues.email.trim()))
+  ) {
+    errors.email = "Please enter a valid email";
+  }
+  if (
+    !formValues.password ||
+    (formValues.password && formValues.password.trim().length < 6)
+  ) {
+    errors.password = "Your password must be atleast six characters";
+  }
+  if (formValues.confirmPassword !== formValues.password) {
+    errors.confirmPassword = "Passwords do not match";
+  }
+  return errors;
+};
 
-export default reduxForm({ form: "RegisterForm" })(RegisterForm);
+export default reduxForm({ validate, form: "RegisterForm" })(RegisterForm);
