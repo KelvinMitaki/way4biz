75b0de6a6d7a81e9cab7a5b2ec204c3440db3292
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-24 17:34:50 +0300

    added responsiveness

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import Market from "../Market/Market";
 
-<<<<<<< HEAD
 class Home extends React.Component {
   render() {
     return (
@@ -12,19 +11,6 @@ class Home extends React.Component {
       </div>
     );
   }
-=======
-import "./Home.css";
-
-class Home extends React.Component{
-    render(){
-        return (
-            <div className="home-section">
-                <Hero/>
-                <Market/>
-            </div>
-        )
-    }
->>>>>>> 04e7bbbd17dc8627a274daea57febcb57090b65b
 }
 
 export default Home;
