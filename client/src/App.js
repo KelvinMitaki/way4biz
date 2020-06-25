4155e317e0ece67d988f84153c72000b674e6722
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-06-25 10:11:58 +0300

    added few changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ import Authenticate from "./components/Authenticate/Authenticate";
 import AddressForm from "./components/Checkout/AddressForm";
 import CheckOut from "./components/Checkout/Checkout";
 import { connect } from "react-redux";
-import CheckOut from "./components/Checkout/Checkout";
 
 class App extends React.Component {
   render() {
