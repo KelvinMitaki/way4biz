8723766fabf198d5abf2a77c002703d7d71ad028
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-25 10:16:02 +0300

    removed new customer

--- client/src/components/Header/User.js
+++ client/src/components/Header/User.js
@@ -1,54 +1,53 @@
 import React from "react";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 import "./User.css";
 
-
 class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            open:false
-        }
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    }
+  handleMouseEnter(e) {
+    this.setState((prevState, props) => {
+      return { open: !prevState.open };
+    });
+  }
 
-    handleMouseEnter(e){
-        this.setState((prevState,props)=>{
-            return{open:!prevState.open}
-        })
-    }
-
-    render(){
-        return (
-            <div id="user-auth" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseEnter}>
-                <div className="icon user-icon flaticon-user">
-                    <span>Sign In | Join</span>
-                </div>
-               {this.state.open ?
-                 <div id="auth-info">
-                     <p>Welcome</p>
-                     <Link to="/sign-in" className="btn btn-md login" >Sign In</Link>
-                     <p>Sign in with:</p>
-                     <div className="auth-btns">
-                         <Link to="/" className="btn btn-md google">Google</Link>
-                         {/* <button className="btn btn-md facebook">Facebook</button> */}
-                     </div>
-                     <p>New Customer?</p>
-                     <Link to="/sign-in" id="register-link">Create Account</Link>
-                     <hr/>
-
-                 </div> 
-                 :
-                 null  
-                }
+  render() {
+    return (
+      <div
+        id="user-auth"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseEnter}
+      >
+        <div className="icon user-icon flaticon-user">
+          <span>Sign In | Join</span>
+        </div>
+        {this.state.open ? (
+          <div id="auth-info">
+            <p>Welcome</p>
+            <Link to="/sign-in" className="btn btn-md login">
+              Sign In
+            </Link>
+            <p>Sign in with:</p>
+            <div className="auth-btns">
+              <Link to="/" className="btn btn-md google">
+                Google
+              </Link>
+              {/* <button className="btn btn-md facebook">Facebook</button> */}
             </div>
-            
-        )
-    }
+
+            <hr />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
-export default User
+export default User;
