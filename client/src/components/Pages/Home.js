80ea1d5b1c9f5705adbb2a5290e3fe1c8502ff36
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-25 09:58:08 +0300

    added checkout link

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Hero from "../Hero/Hero";
 import Market from "../Market/Market";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 class Home extends React.Component {
   render() {
-    console.log(this.props.isSignedIn);
-    console.log(this.props);
-
     return (
       <div>
         <Hero />
