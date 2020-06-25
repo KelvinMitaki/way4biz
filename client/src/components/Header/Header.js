adbe2f7a7a30f26e7dd4f2f02533e8c030755ff1
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-25 10:19:51 +0300

    remved semicollon

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,22 +1,19 @@
 import React from "react";
-
 import "./Header.css";
-
 import Logo from "./Logo";
 import Search from "./Search";
 import Icons from "./Icons";
 
 class Header extends React.Component {
-    render(){
-        return(
-            <div className="header d-flex">
-                <Logo/>
-                <Search/>
-                <Icons/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="header d-flex">
+        <Logo />
+        <Search />
+        <Icons />
+      </div>
+    );
+  }
 }
 
-
-export default Header
+export default Header;
