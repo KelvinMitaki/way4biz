b4c9d0002c270b06476a94f4853a38800f4d3b2c
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-30 10:55:11 +0300

    added a seller register post route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,142 @@
+const nodeMailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const route = require("express").Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
 const Product = require("../models/Product");
 const isSeller = require("../middlewares/is-seller");
+const Seller = require("../models/Seller");
+const User = require("../models/User");
 
-const route = require("express").Router();
+const transporter = nodeMailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY
+    }
+  })
+);
+
+route.post(
+  "/api/seller/register",
+  check("firstName")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage(
+      "Please enter your first name with a minimum of three characters"
+    ),
+  check("lastName")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage(
+      "Please enter your last name with a minimum of three characters"
+    ),
+  check("email").trim().isEmail().withMessage("Please enter a valid email"),
+  check("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("Please enter a password with a minimum of six characters"),
+  check("description")
+    .trim()
+    .isLength({ min: 20 })
+    .withMessage("Please enter a description with a minimum of 20 characters"),
+  check("storeName")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Please enter a store name with 3 or more characters"),
+  check("phoneNumber")
+    .isNumeric()
+    .withMessage("Please enter a valid phone number"),
+  check("city").trim().not().isEmpty().withMessage("Please enter a valid city"),
+  check("streetAddress")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Please enter a valid street address"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(401).send(errors.array()[0].msg);
+      }
+      const {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName,
+        phoneNumber,
+        description,
+        storeName,
+        city,
+        streetAddress
+      } = req.body;
+      if (password !== confirmPassword) {
+        return res.status(401).send({ message: "Passwords do not match" });
+      }
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        return res.status(401).send("A user with that email already exists");
+      }
+      const sellerExists = await Seller.findOne({ email });
+      if (sellerExists) {
+        return res
+          .status(401)
+          .send({ message: "A seller with that email already exists" });
+      }
+      // **TODO**  CHECK IF EMAIL IS VALID VIA SENDGRID
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const seller = new Seller({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        phoneNumber,
+        description,
+        storeName,
+        city,
+        streetAddress
+      });
+      const token = jwt.sign(
+        { _id: seller._id },
+        process.env.CONFIRM_EMAIL_JWT,
+        {
+          expiresIn: "1 hour"
+        }
+      );
+      // **TODO** FROM EMAIL TO BE CHANGED
+      transporter.sendMail(
+        {
+          to: email,
+          from: "kevinkhalifa911@gmail.com",
+          subject: "Email Confirmation",
+          html: `<html lang="en">
+      <body>
+          <h5 style="font-family: Arial, Helvetica, sans-serif;">Confirming Your Email</h5>
+          <p style="font-family: Arial, Helvetica, sans-serif;">Please Click
+              <a href=${process.env.EMAIL_CONFIRM_REDIRECT}/${token}>here</a> to confirm your email
+          </p>
+      </body>
+      </html>`
+        },
+        (error, info) => {
+          if (error) {
+            console.log(error);
+          }
+          console.log(info);
+        }
+      );
+      await seller.save();
+      res.status(201).send({
+        message:
+          "An email has been sent to your email address, please check it to confirm your account"
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 route.get("/api/products/:sellerId", isSeller, async (req, res) => {
   try {
     const { sellerId } = req.params;
