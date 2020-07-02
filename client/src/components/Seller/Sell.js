1c4ead9979d489838f6e175468455b5e6dee89b2
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-02 22:26:34 +0300

    removed unused imports on seller component

--- client/src/components/Seller/Sell.js
+++ client/src/components/Seller/Sell.js
@@ -5,8 +5,6 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import validator from "validator";
 import { registerSeller, addProduct } from "../../redux/actions";
-import AuthHeader from "../Authenticate/AuthHeader";
-import SelectField from "../Checkout/SelectField";
 import SellerDashBoardHeader from "./SellerDashBoardHeader";
 import SellerTextArea from "../Account/SellerTextArea";
 import SellerDropDown from "./SellerDropDown";
