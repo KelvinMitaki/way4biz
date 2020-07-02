6f506e1a6b691c317da3658b92f55390ce5a01fe
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-03 01:09:58 +0300

    updated cart icon number

--- client/src/components/Header/Cart.js
+++ client/src/components/Header/Cart.js
@@ -2,17 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Cart.css";
+import { connect } from "react-redux";
 
 class Cart extends React.Component {
   render() {
     return (
       <Link to="/cart" className="secondary-link">
         <div className="icon cart-icon flaticon-shopping-cart">
-          <span className="badge">0</span>
+          <span className="badge">{this.props.cart.length}</span>
         </div>
       </Link>
     );
   }
 }
-
-export default Cart;
+const mapStateToProps = state => {
+  return {
+    cart: state.cartReducer.cart
+  };
+};
+export default connect(mapStateToProps)(Cart);
