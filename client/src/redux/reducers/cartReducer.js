94911529658840b637258719d4bd733ca3e671ff
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-03 02:05:52 +0300

    wiredup adding product to cart

--- client/src/redux/reducers/cartReducer.js
+++ client/src/redux/reducers/cartReducer.js
@@ -8,7 +8,24 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return { ...state, cart: [...state.cart, action.payload] };
+      const productExists = state.cart.find(
+        product => product._id.toString() === action.payload._id.toString()
+      );
+      if (productExists) {
+        return {
+          ...state,
+          cart: state.cart.map(item =>
+            item._id.toString() === action.payload._id.toString()
+              ? { ...action.payload, quantity: item.quantity + 1 }
+              : item
+          )
+        };
+      }
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, quantity: 1 }]
+      };
+
     default:
       return state;
   }
