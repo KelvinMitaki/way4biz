53a05f0bf6e3234247db4527fa8ce0f7d529311c
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-06 02:01:42 +0300

    Added arrow to categories and tested browser compatibility

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IconContext } from "react-icons";
-import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 
 import "./Products.css";
 import Header from "../Header/Header";
