d73bfb27e83cb07442cfe3f995260dce825f14dc
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-07 01:51:57 +0300

    Modified seller dasboard tabs

--- client/src/components/Seller/SellerProducts.js
+++ client/src/components/Seller/SellerProducts.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
-import Accordion from "react-bootstrap/Accordion";
-import { Card, Button } from "react-bootstrap";
-// import Tabs from "react-responsive-tabs";
+import Tabs from "react-responsive-tabs";
 
 import "./SellerProducts.css";
 
@@ -11,12 +7,30 @@ import SellerDashBoardMenu from "./SellerDashBoardMenu";
 import SellerDashBoardHeader from "./SellerDashBoardHeader";
 import { fetchSellerProducts } from "../../redux/actions";
 import DashBoardProduct from "./DashBoardProduct";
-import DashBoardProductMediumScreen from "./DashBoardProductMediumScreens";
 import { connect } from "react-redux";
 
 class SellerProducts extends React.Component {
+  state = {};
   componentDidMount() {
     this.props.fetchSellerProducts();
+  }
+
+  getTabs() {
+    let tabs = [
+      { title: "Total Products", data: <DashBoardProduct category="total" /> },
+      { title: "Live On Site", data: <DashBoardProduct category="live" /> },
+      { title: "Under Review", data: <DashBoardProduct category="review" /> },
+      { title: "Rejected", data: <DashBoardProduct category="rejected" /> },
+      { title: "Sold Out", data: <DashBoardProduct category="sold-out" /> },
+    ];
+
+    return tabs.map((tab, index) => ({
+      title: tab.title,
+      getContent: () => tab.data,
+      key: index,
+      tabClassName: "tab",
+      panelClassName: "seller-db-panel",
+    }));
   }
   render() {
     return (
@@ -29,220 +43,14 @@ class SellerProducts extends React.Component {
           <div className="col-lg-9 mt-5">
             <div className="container">
               <div className="row">
-                <div id="dashboard-products-lg-screen" className="col-lg-12">
+                <div className="col-lg-12">
                   <Tabs
-                    defaultActiveKey="total-products"
-                    id="uncontrolled-tab-example"
-                    className="tab"
-                  >
-                    <Tab
-                      tabClassName="my-tab"
-                      eventKey="total-products"
-                      title="Total Products"
-                    >
-                      <div className="container">
-                        <div className="row no-gutters dashboard-product-titles">
-                          <div className="col d-flex mt-4 mb-2">
-                            <h6
-                              className="col-lg-5"
-                              style={{ textAlign: "left" }}
-                            >
-                              Item
-                            </h6>
-                            <h6 className="col-lg-2">Quantity</h6>
-                            <h6 className="col-lg-2">Price</h6>
-                            <h6 className="col-lg-2">Status</h6>
-                            <h6 className="col-lg-1"> </h6>
-                          </div>
-                        </div>
-                      </div>
-                      <DashBoardProduct />
-                    </Tab>
-                    <Tab
-                      eventKey="live-on-site"
-                      title="Live On Site"
-                      tabClassName="my-tab"
-                    >
-                      <div className="container">
-                        <div className="row no-gutters dashboard-product-titles">
-                          <div className="col d-flex mt-4 mb-2">
-                            <h6
-                              className="col-lg-5"
-                              style={{ textAlign: "left" }}
-                            >
-                              Item
-                            </h6>
-                            <h6 className="col-lg-2">Quantity</h6>
-                            <h6 className="col-lg-2">Price</h6>
-                            <h6 className="col-lg-2">Status</h6>
-                            <h6 className="col-lg-1"> </h6>
-                          </div>
-                        </div>
-                      </div>
-                      <DashBoardProduct />
-                    </Tab>
-                    <Tab
-                      eventKey="under-preview"
-                      title="Under Review"
-                      tabClassName="my-tab"
-                    >
-                      <div className="container">
-                        <div className="row no-gutters dashboard-product-titles">
-                          <div className="col d-flex mt-4 mb-2">
-                            <h6
-                              className="col-lg-5"
-                              style={{ textAlign: "left" }}
-                            >
-                              Item
-                            </h6>
-                            <h6 className="col-lg-2">Quantity</h6>
-                            <h6 className="col-lg-2">Price</h6>
-                            <h6 className="col-lg-2">Status</h6>
-                            <h6 className="col-lg-1"> </h6>
-                          </div>
-                        </div>
-                      </div>
-                      <DashBoardProduct />
-                    </Tab>
-                    <Tab
-                      eventKey="rejected"
-                      title="Rejected"
-                      tabClassName="my-tab"
-                    >
-                      <div className="container">
-                        <div className="row no-gutters dashboard-product-titles">
-                          <div className="col d-flex mt-4 mb-2">
-                            <h6
-                              className="col-lg-5"
-                              style={{ textAlign: "left" }}
-                            >
-                              Item
-                            </h6>
-                            <h6 className="col-lg-2">Quantity</h6>
-                            <h6 className="col-lg-2">Price</h6>
-                            <h6 className="col-lg-2">Status</h6>
-                            <h6 className="col-lg-1"> </h6>
-                          </div>
-                        </div>
-                      </div>
-                      <DashBoardProduct />
-                    </Tab>
-                    <Tab
-                      eventKey="sold-out"
-                      title="Sold Out"
-                      tabClassName="my-tab"
-                    >
-                      <div className="container">
-                        <div className="row no-gutters dashboard-product-titles">
-                          <div className="col d-flex mt-4 mb-2">
-                            <h6
-                              className="col-lg-5"
-                              style={{ textAlign: "left" }}
-                            >
-                              Item
-                            </h6>
-                            <h6 className="col-lg-2">Quantity</h6>
-                            <h6 className="col-lg-2">Price</h6>
-                            <h6 className="col-lg-2">Status</h6>
-                            <h6 className="col-lg-1"> </h6>
-                          </div>
-                        </div>
-                      </div>
-                      <DashBoardProduct />
-                    </Tab>
-                  </Tabs>
-                </div>
-                <div
-                  id="dashboard-products-sm-screen"
-                  className="col-md-10 mx-auto"
-                >
-                  <Accordion defaultActiveKey="0">
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle
-                          as={Button}
-                          variant="link"
-                          eventKey="0"
-                          className="accordion-toggle-text"
-                        >
-                          Total Products
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="0">
-                        <Card.Body className="card-products">
-                          <DashBoardProductMediumScreen />
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle
-                          as={Button}
-                          variant="link"
-                          eventKey="1"
-                          className="accordion-toggle-text"
-                        >
-                          Live On Site
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="1">
-                        <Card.Body className="card-products">
-                          <DashBoardProductMediumScreen />
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle
-                          as={Button}
-                          variant="link"
-                          eventKey="2"
-                          className="accordion-toggle-text"
-                        >
-                          Under Review
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="2">
-                        <Card.Body className="card-products">
-                          <DashBoardProductMediumScreen />
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle
-                          as={Button}
-                          variant="link"
-                          eventKey="3"
-                          className="accordion-toggle-text"
-                        >
-                          Rejected
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="3">
-                        <Card.Body className="card-products">
-                          <DashBoardProductMediumScreen />
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle
-                          as={Button}
-                          variant="link"
-                          eventKey="4"
-                          className="accordion-toggle-text"
-                        >
-                          Sold Out
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="4">
-                        <Card.Body className="card-products">
-                          <DashBoardProductMediumScreen />
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                  </Accordion>
+                    items={this.getTabs()}
+                    transformWidth={960}
+                    transform={true}
+                    showMoreLabel={"More..."}
+                    showInkBar={true}
+                  />
                 </div>
               </div>
             </div>
