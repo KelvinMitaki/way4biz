a5e1e386fb9ac12cbf05cbfd38ec2f09d65fb7ca
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-09 01:40:04 +0300

    fixed css grid

--- client/src/components/Market/Market.js
+++ client/src/components/Market/Market.js
@@ -19,73 +19,53 @@ class Market extends React.Component {
         <div className="col market-head">
           <h1>Selling</h1>
         </div>
-        <div className="products-section">
-          <InfiniteScroll
-            dataLength={this.props.products.length}
-            next={this.fetchMoreData}
-            hasMore={this.props.hasMore}
-            loader={<h4>Loading...</h4>}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>You've seen it all</b>
-              </p>
-            }
-          >
-            {this.props.products.length !== 0 &&
-              this.props.products.map(product => (
-                <div className="product" key={product._id}>
-                  <Link
-                    key={product._id}
-                    to={`/product/${product._id}`}
-                    title={product.name}
-                    className="product-link"
-                  >
-                    <img src={product.imageUrl} alt={product.name} />
-                    <div style={{ padding: "0px 10px" }}>
-                      <p className="product-name">{product.name}</p>
-                      <p style={{ fontWeight: "bolder" }} className="price">
-                        Ksh.{product.price.toLocaleString()}
-                      </p>
-                    </div>
-                  </Link>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "0px 10px"
-                    }}
-                    className="my-2"
-                  >
-                    <Heart product={product} />
-                    {product.freeShipping && (
-                      <p className="lead" style={{ fontSize: "smaller" }}>
-                        Free Shipping
-                      </p>
-                    )}
+        <InfiniteScroll
+          className="products-section"
+          dataLength={this.props.products.length}
+          next={this.fetchMoreData}
+          hasMore={this.props.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>You've seen it all</b>
+            </p>
+          }
+        >
+          {this.props.products.length !== 0 &&
+            this.props.products.map(product => (
+              <div className="product" key={product._id}>
+                <Link
+                  key={product._id}
+                  to={`/product/${product._id}`}
+                  title={product.name}
+                  className="product-link"
+                >
+                  <img src={product.imageUrl} alt={product.name} />
+                  <div style={{ padding: "0px 10px" }}>
+                    <p className="product-name">{product.name}</p>
+                    <p style={{ fontWeight: "bolder" }} className="price">
+                      Ksh.{product.price.toLocaleString()}
+                    </p>
                   </div>
-
-                  <div style={{ height: "10px", padding: "0px 10px" }}>
-                    {product.freeShipping && (
-                      <p className="lead" style={{ fontSize: "smaller" }}>
-                        Free Shipping
-                      </p>
-                    )}
-                  </div>
-
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "0px 10px"
-                    }}
-                    className="my-2"
-                  >
-                    <Heart product={product} />
-                  </div>
+                </Link>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    padding: "0px 10px"
+                  }}
+                  className="my-2"
+                >
+                  <Heart product={product} />
+                  {product.freeShipping && (
+                    <p className="lead" style={{ fontSize: "smaller" }}>
+                      Free Shipping
+                    </p>
+                  )}
                 </div>
-              ))}
-          </InfiniteScroll>
-        </div>
+              </div>
+            ))}
+        </InfiniteScroll>
       </div>
     );
   }
