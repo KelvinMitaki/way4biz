395c827977a371824a153c3c9833e5848c5c7516
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-12 14:47:12 +0300

    Did nothing

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -15,7 +15,7 @@ import {
   addToWishlist,
   removeFromWishlist,
   fetchSingleProduct,
-  fetchRelatedProducts
+  fetchRelatedProducts,
 } from "../../redux/actions";
 import { IconContext } from "react-icons/lib";
 import ProductSecondaryDetails from "./ProductSecondaryDetails";
@@ -27,7 +27,7 @@ class Product extends React.Component {
     super(props);
     this.state = {
       modalShow: false,
-      clicked: false
+      clicked: false,
       // imgUrl: this.props.product.imageUrl,
     };
     this.handleClick = this.handleClick.bind(this);
@@ -45,9 +45,9 @@ class Product extends React.Component {
   }
   handleClick(e) {
     e.preventDefault();
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return {
-        modalShow: !prevState.modalShow
+        modalShow: !prevState.modalShow,
       };
     });
     const { product, addToCart } = this.props;
@@ -56,9 +56,9 @@ class Product extends React.Component {
 
   handleCloseModal(e) {
     e.preventDefault();
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return {
-        modalShow: !prevState.modalShow
+        modalShow: !prevState.modalShow,
       };
     });
   }
@@ -69,14 +69,14 @@ class Product extends React.Component {
       smallImage: {
         alt: product.name,
         isFluidWidth: true,
-        src: product.imageUrl
+        src: product.imageUrl,
       },
       largeImage: {
         src: product.imageUrl,
         width: 1200,
-        height: 1800
+        height: 1800,
       },
-      enlargedImageContainerStyle: { background: "#fff", zIndex: 9 }
+      enlargedImageContainerStyle: { background: "#fff", zIndex: 9 },
     };
   }
 
@@ -84,12 +84,13 @@ class Product extends React.Component {
     const stockQuantity =
       this.props.product && this.props.product.stockQuantity;
     const itemInWishlist = this.props.wishlist.find(
-      item => item._id === this.props.product && this.props.product._id
+      (item) => item._id === this.props.product && this.props.product._id
     );
     let itemInCart = false;
     itemInCart = this.props.cart.find(
-      item => item._id === this.props.product && this.props.product._id
+      (item) => item._id === this.props.product && this.props.product._id
     );
+
     if (!this.props.product) return <ScreenLoader />;
     return (
       <React.Fragment>
@@ -207,7 +208,7 @@ class Product extends React.Component {
                         clickable={false}
                         value={Math.round(
                           this.props.productReviews
-                            .map(p => p.rating)
+                            .map((p) => p.rating)
                             .reduce((acc, cur) => acc + cur, 0) /
                             this.props.productReviews.length
                         )}
@@ -258,7 +259,7 @@ class Product extends React.Component {
               <h3>Related Products</h3>
               <div className="related-products-wrapper">
                 {this.props.relatedProducts.length !== 0 &&
-                  this.props.relatedProducts.map(item => (
+                  this.props.relatedProducts.map((item) => (
                     <a key={item._id} href={`/product/${item._id}`}>
                       <div key={item._id} className="related-product">
                         <img src={item.imageUrl} alt={item.name} />
@@ -297,7 +298,7 @@ const mapStateToProps = (state, ownProps) => {
     wishlist: state.cartReducer.wishlist,
     cart: state.cartReducer.cart,
     relatedProducts: state.product.relatedProducts,
-    productReviews: state.product.productReviews
+    productReviews: state.product.productReviews,
   };
 };
 export default withRouter(
@@ -306,6 +307,6 @@ export default withRouter(
     addToWishlist,
     removeFromWishlist,
     fetchSingleProduct,
-    fetchRelatedProducts
+    fetchRelatedProducts,
   })(Product)
 );
