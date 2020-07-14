4d1b3f6548d485a7f92887919dae5b241ce22bef
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-14 14:31:36 +0300

    wired up filtering and sorting for mobile

--- client/src/redux/reducers/filterReducer.js
+++ client/src/redux/reducers/filterReducer.js
@@ -10,7 +10,7 @@ const INITIAL_STATE = {
   priceMin: null,
   rating: false,
   freeShipping: false,
-  price: "lowestPrice"
+  price: null
 };
 
 export default (state = INITIAL_STATE, action) => {
