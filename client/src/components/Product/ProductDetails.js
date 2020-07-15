70635c41b9cd3862fe277f17d9c6a03e6fd7e5b7
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-15 15:31:28 +0300

    Modified styles on seller dashboard

--- client/src/components/Product/ProductDetails.js
+++ client/src/components/Product/ProductDetails.js
@@ -1,10 +1,15 @@
 import React from "react";
 import renderHTML from "react-render-html";
+import "./ProductDetails.css";
 class ProductDetails extends React.Component {
   render() {
     return (
       <div style={{ width: "100%", backgroundColor: "#fff", padding: "10px" }}>
-        <p dangerouslySetInnerHTML={{ __html: this.props.data }} />
+        <p
+          className="dangerous-stuff"
+          dangerouslySetInnerHTML={{ __html: this.props.data }}
+          style={{ width: "100% !important", overflow: "hidden" }}
+        />
       </div>
     );
   }
