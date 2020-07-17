81493d5d37ce3b32782eb6aa8324f67618eef711
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-17 14:17:36 +0300

    fixed image fetching from s3 when served from the db

--- client/src/components/Seller/ProductImageUploadsContainer.js
+++ client/src/components/Seller/ProductImageUploadsContainer.js
@@ -15,7 +15,7 @@ class ProductImageUploadsContainer extends React.Component {
       <div className="uploads-container box-container">
         {/* mapping here */}
         {this.props.imageUrl.length !== 0 &&
-          this.props.imageUrl.map((url) => (
+          this.props.imageUrl.map(url => (
             <div key={url} className={`uploaded-product-image-wrapper`}>
               <div className="uploaded-product-image">
                 <img
@@ -36,11 +36,15 @@ class ProductImageUploadsContainer extends React.Component {
           ))}
         {this.props.images &&
           this.props.images.length !== 0 &&
-          this.props.images.map((url) => (
+          this.props.images.map(url => (
             <div key={url} className="uploaded-product-image-wrapper">
               <div className="uploaded-product-image">
                 <img
-                  src={`https://e-commerce-gig.s3.eu-west-2.amazonaws.com/${url}`}
+                  src={
+                    url.includes("http")
+                      ? url
+                      : `https://e-commerce-gig.s3.eu-west-2.amazonaws.com/${url} `
+                  }
                   alt={url}
                 />
               </div>
@@ -61,10 +65,10 @@ class ProductImageUploadsContainer extends React.Component {
     );
   }
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     imageUrl: state.image.imageUrl,
-    deleteImageLoading: state.image.deleteImageLoading,
+    deleteImageLoading: state.image.deleteImageLoading
   };
 };
 export default withRouter(
