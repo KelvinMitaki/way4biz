8de3285fe199a1c6c58b3e8052222448d9c75e3b
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-20 11:44:30 +0300

    Added admin dsahboard seller component

---
+++ client/src/components/Admin/AdminDashBoardSeller.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./AdminDashBoardSeller.css";
+import DashBoardHeader from "./AdminDashBoardHeader";
+import SecondaryHeader from "./AdminDashboardSecondaryHeader";
+import { IconContext } from "react-icons";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+class AdminDashBoardSeller extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid p-0">
+        <DashBoardHeader />
+        <SecondaryHeader />
+        <div className="mt-4 container">
+          <div className="box-container">
+            <IconContext.Provider value={{ className: "arrow-icon ml-3 my-2" }}>
+              <div className="d-flex align-items-center">
+                <Link to="/admin-sellers">
+                  <BsArrowLeft />
+                </Link>
+                <h3 className="ml-3">Seller ID</h3>
+              </div>
+            </IconContext.Provider>
+            <div className="admin-individual-seller-details">
+              <h6>
+                <strong>Name: </strong>John Doe
+              </h6>
+              <h6>
+                <strong>StoreName:</strong>Limuru Bata
+              </h6>
+              <h6>
+                <strong>Date Joined:</strong>1/1/2000
+              </h6>
+            </div>
+            {/* <div className=""></div> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminDashBoardSeller;
