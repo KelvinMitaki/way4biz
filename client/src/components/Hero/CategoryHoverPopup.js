29ee4cf52542ab88262c3b88e7f7812dd2484e36
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-22 18:33:47 +0300

    Added catehory hover popup component

---
+++ client/src/components/Hero/CategoryHoverPopup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./CategoryHoverPopup.css";
+
+class CategoryHoverPopup extends React.Component {
+  render() {
+    return <div className="category-hover-popup"></div>;
+  }
+}
+
+export default CategoryHoverPopup;
