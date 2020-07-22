462ada302a7887dd65b0bcfa14d6f222c5abf562
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-22 19:49:42 +0300

    DS implementation

--- client/src/components/Admin/LineGraph.js
+++ client/src/components/Admin/LineGraph.js
@@ -16,7 +16,7 @@ class LineGraph extends React.Component {
         responsive: true,
         title: {
           display: true,
-          text: "Daily Sales"
+          text: "Daily Sales",
         },
         scales: {
           yAxes: [
@@ -24,11 +24,11 @@ class LineGraph extends React.Component {
               ticks: {
                 min: 0,
                 max: 200,
-                stepSize: 50
-              }
-            }
-          ]
-        }
+                stepSize: 50,
+              },
+            },
+          ],
+        },
       },
       data: {
         labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
@@ -37,55 +37,93 @@ class LineGraph extends React.Component {
             data: [86, 114, 10, 100],
             label: "Sales",
             borderColor: "#f76b1a",
-            fill: false
-          }
-        ]
-      }
+            fill: false,
+          },
+        ],
+      },
     });
   }
   render() {
-    const test = this.props.weeklySales.map(sale => ({
+    const test = this.props.weeklySales.map((sale) => ({
       day: new Date(sale.createdAt).getDay(),
-      items: sale.items
+      items: sale.items,
     }));
     console.log(test);
+    const possibleWeekArrangements = [
+      ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"],
+      ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue"],
+      ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+      ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu"],
+      ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
+    ];
     const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const d = test.map(t => {
+    const d = test.map((t) => {
       const te = days.filter((day, index) => index === t.day);
       return {
         ...te,
         items: t.items
-          .map(ite => ite.quantity)
-          .reduce((acc, cur) => acc + cur, 0)
+          .map((ite) => ite.quantity)
+          .reduce((acc, cur) => acc + cur, 0),
       };
     });
     // .reduce((acc, cur) => acc.concat(cur), []);
-    console.log("d", d);
+    // console.log("d", d);
     const lookup = d.reduce((acc, cur) => {
       acc[cur["0"]] = ++acc[cur["0"]] || 0;
       return acc;
     }, {});
-    const duplicates = d.filter(e => lookup[e["0"]]);
+    const duplicates = d.filter((e) => lookup[e["0"]]);
 
     if (duplicates.length !== 0) {
       const sumDuplicates = duplicates
-        .map(dup => dup.items)
+        .map((dup) => dup.items)
         .reduce((acc, cur) => acc + cur, 0);
       const filteredDup = { 0: duplicates[0]["0"], items: sumDuplicates };
 
-      const withoutDup = d.filter(items => items["0"] !== filteredDup["0"]);
+      const withoutDup = d.filter((items) => items["0"] !== filteredDup["0"]);
       const newArr = [...withoutDup, filteredDup];
 
       console.log("newArr", newArr);
 
-      const daysWithoutOrders = days.filter(day => {
-        const dayFound = newArr.find(d => d["0"] === day);
+      var myWeek;
+
+      for (var i = 0; i < possibleWeekArrangements.length; i++) {
+        //O(1) best case O(7) worst case
+        if (possibleWeekArrangements[i][0] === newArr[0][0]) {
+          myWeek = possibleWeekArrangements[i];
+          break;
+        }
+      }
+
+      console.log("my week", myWeek);
+
+      var desiredArray = [];
+
+      for (var i = 0; i < myWeek.length; i++) {
+        //O(7) best and worst case
+        if (newArr[0][0] === myWeek[i]) {
+          var data = [myWeek[i], newArr[0].items];
+          newArr.shift();
+          desiredArray.push(data);
+        } else {
+          var data = [myWeek[i], 0];
+          desiredArray.push(data);
+        }
+      }
+
+      console.log("desiredArray", desiredArray);
+      // take the desired array to the linegraph.
+
+      const daysWithoutOrders = days.filter((day) => {
+        const dayFound = newArr.find((d) => d["0"] === day);
         if (dayFound) {
           return false;
         }
         return true;
       });
-      const daysArrObj = daysWithoutOrders.map(day => ({ 0: day, items: 0 }));
+      const daysArrObj = daysWithoutOrders.map((day) => ({ 0: day, items: 0 }));
       const allDays = [...newArr, ...daysArrObj];
       console.log(allDays);
     }
@@ -97,9 +135,9 @@ class LineGraph extends React.Component {
     );
   }
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    weeklySales: state.product.weeklySales
+    weeklySales: state.product.weeklySales,
   };
 };
 export default connect(mapStateToProps)(LineGraph);
