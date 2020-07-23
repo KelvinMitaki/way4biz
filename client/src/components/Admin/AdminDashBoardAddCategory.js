e53a1d15c30351dcdf23937802a38730ddb82b50
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-23 14:05:43 +0300

    added ability to add a subcategory

--- client/src/components/Admin/AdminDashBoardAddCategory.js
+++ client/src/components/Admin/AdminDashBoardAddCategory.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { reduxForm, Field } from "redux-form";
+
 import "./AdminDashBoardAddCategory.css";
 import AdminDashBoardHeader from "./AdminDashBoardHeader";
 import AdminDashboardSecondaryHeader from "./AdminDashboardSecondaryHeader";
+import { IconContext } from "react-icons/lib";
+import { AiOutlineSearch } from "react-icons/ai";
 
 class AdminDashBoardAddCategory extends React.Component {
+  state = {
+    subcategories: [],
+    typing: ""
+  };
+  handleChange = e => {
+    this.setState({ typing: e.target.value });
+  };
+  handleTypingSubmit = e => {
+    if (this.state.typing !== "") {
+      return this.setState({
+        subcategories: [...this.state.subcategories, this.state.typing],
+        typing: ""
+      });
+    }
+  };
   render() {
     return (
       <div className="container-fluid p-0">
@@ -17,18 +34,7 @@ class AdminDashBoardAddCategory extends React.Component {
             </h3>
             <div className="container">
               <div className="form-group">
-                <Field
-                  type="text"
-                  name="firstName"
-                  label="Category"
-                  component={FormField}
-                />
-                <Field
-                  type="text"
-                  name="firstName"
-                  label="Sub Categories"
-                  component={FormField}
-                />
+                <label htmlFor="add-category">Category</label>
                 <input
                   className="form-control"
                   type="text"
@@ -36,11 +42,48 @@ class AdminDashBoardAddCategory extends React.Component {
                   id="add-category"
                 />
                 <label htmlFor="sub-categories">Sub Categories</label>
-                <input
-                  id="sub-categories"
-                  className="form-control"
-                  placeholder="eg:phones"
-                />
+                <div className="input-group">
+                  <input
+                    type="text"
+                    onChange={this.handleChange}
+                    className="form-control header-input-search"
+                    placeholder="e.g iPhone"
+                    value={this.state.typing}
+                  />
+                  <div
+                    className="input-group-append"
+                    onClick={this.handleTypingSubmit}
+                  >
+                    <button
+                      id="header-search-btn"
+                      disabled={this.state.typing === ""}
+                    >
+                      <div className="icon-container">
+                        <span>Enter</span>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+                {this.state.subcategories.length !== 0 &&
+                  this.state.subcategories.map((sub, index) => (
+                    <React.Fragment key={index}>
+                      <p>
+                        {sub}{" "}
+                        <strong
+                          style={{ cursor: "pointer" }}
+                          onClick={() =>
+                            this.setState({
+                              subcategories: this.state.subcategories.filter(
+                                (s, i) => i !== index
+                              )
+                            })
+                          }
+                        >
+                          X
+                        </strong>
+                      </p>
+                    </React.Fragment>
+                  ))}
               </div>
             </div>
           </div>
@@ -50,6 +93,4 @@ class AdminDashBoardAddCategory extends React.Component {
   }
 }
 
-export default reduxForm({ validate, form: "AdminDashBoardAddCategory" })(
-  AdminDashBoardAddCategory
-);
+export default AdminDashBoardAddCategory;
