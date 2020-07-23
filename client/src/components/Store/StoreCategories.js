a89acb81304f89531c2a1b32b0d0df5ccbdf800f
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-23 19:07:52 +0300

    Added store categories component

---
+++ client/src/components/Store/StoreCategories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./StoreCategories.css";
+import Store from "./Store";
+
+class StoreCategories extends React.Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default StoreCategories;
