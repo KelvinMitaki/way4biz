c1bc727d667e16ce1983e5e06e07109f800cec41
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-24 00:36:15 +0300

    Styled order items component

--- client/src/components/Admin/AdminDashBoardOrderItems.js
+++ client/src/components/Admin/AdminDashBoardOrderItems.js
@@ -11,44 +11,45 @@ class AdminDashBoardOrderItems extends React.Component {
         <AdminDashBoardHeader />
         <AdminDashboardSecondaryHeader />
         <div className="container mt-4">
-          <div className="box-container">
+          <div className="box-container admin-order-items-wrapper">
             <h3 style={{ textAlign: "center" }} className="my-2">
               Ordered Items
             </h3>
             <div className="container">
-              <div className="row y">
-                <div className="col-md-3">
+              <div className="row y order-title">
+                {/* <div className="col-md-3">
                   <h6>Buyer</h6>
-                </div>
+                </div> */}
                 <div className="col-md-3">
                   <h6 style={{ paddingLeft: "20%" }}>Item</h6>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-5">
                   <h6>Item Info</h6>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <h6>Seller</h6>
                 </div>
               </div>
               <div className="box-container individual-order-item">
+                {/* mapping here */}
                 <div className="row align-items-center">
-                  <div className="col-md-3">
+                  {/* <div className="col-md-3">
                     <p>
                       <strong className="x mr-1">Buyer:</strong>John Doe
                     </p>
                     <p>
                       <Link to="/admin/buyer-info">View Buyer</Link>
                     </p>
-                  </div>
+                  </div> */}
                   <div className="col-md-3">
                     <img width={"100px"} src="/1.jpg" />
                   </div>
-                  <div className="col-md-3">
-                    <p>Great Beer</p>
+                  <div className="col-md-5">
+                    <p>Great Beer Great Beer Great Beer Great Beer</p>
                     <p>Ksh.1500</p>
                     <p>Qty:10</p>
                   </div>
-                  <div className="col-md-3">
+                  <div className="col-md-4 p-0">
                     <p>
                       <strong className="x mr-1">Seller:</strong>Jane Doe
                     </p>
