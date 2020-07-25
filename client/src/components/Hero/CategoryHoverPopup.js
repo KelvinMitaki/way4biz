a762deeb4aca1a01cb1aef3555b11c9e88b7bf5b
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-25 21:32:44 +0300

    Added sub category popup markup

--- client/src/components/Hero/CategoryHoverPopup.js
+++ client/src/components/Hero/CategoryHoverPopup.js
@@ -1,10 +1,120 @@
 import React from "react";
 
 import "./CategoryHoverPopup.css";
+import { Link } from "react-router-dom";
 
 class CategoryHoverPopup extends React.Component {
   render() {
-    return <div className="category-hover-popup"></div>;
+    return (
+      <div className="category-hover-popup">
+        <div className="hovered-sub-categories-section-wrapper">
+          {/* mapping here */}
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hovered-sub-categories-section">
+            <h5 className="sub-category-title">Helloo World</h5>
+            <ul>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+              <li>
+                <Link to="/">Test</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
