23ec0ec153c289dd41362c317d5b7e5d612b1192
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-26 12:35:26 +0300

    Added uploaded image section in seller uploads

--- client/src/components/Seller/SellerDocuments.js
+++ client/src/components/Seller/SellerDocuments.js
@@ -1,8 +1,9 @@
 import React from "react";
-import PhotosPage from "./PhotosPage";
 import { animateScroll as scroll } from "react-scroll";
 import { connect } from "react-redux";
 import SellerImage from "./SellerImage";
+import "./SellerDocuments.css";
+import { FaTrashAlt } from "react-icons/fa";
 
 class SellerDocuments extends React.Component {
   state = {
@@ -30,8 +31,30 @@ class SellerDocuments extends React.Component {
         <h3>Please upload valid copies of the following documents.</h3>
         <h4 className="my-2">National ID</h4>
         <SellerImage aspectRatio={[3, 2]} />
+        <div className="seller-uploads-wrapper">
+          <div>
+            <img src="/1.jpg" />
+            <div className="seller-uploads-trash-button-wrapper">
+              <button className="btn seller-uploads-trash-button">
+                <FaTrashAlt />
+                <span className="ml-1">Delete</span>
+              </button>
+            </div>
+          </div>
+        </div>
         <h4 className="my-2">Profile Photo</h4>
         <SellerImage />
+        <div className="seller-uploads-wrapper my-2">
+          <img src="/1.jpg" />
+          <div className="seller-uploads-trash-button-wrapper">
+            <button className="btn seller-uploads-trash-button">
+              <FaTrashAlt />
+              <span className="ml-1">Delete</span>
+            </button>
+          </div>
+        </div>
+
+        <h3>Helloo World</h3>
       </div>
     );
   }
