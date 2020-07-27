13f27d80e5e253f73428501f8189adc460a17d22
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-27 16:57:54 +0300

    Added logout for admin

--- client/src/components/Admin/AdminDashboardSecondaryHeader.js
+++ client/src/components/Admin/AdminDashboardSecondaryHeader.js
@@ -1,16 +1,17 @@
 import React from "react";
 import HamburgerMenu from "react-hamburger-menu";
 import "./AdminDashboardSecondaryHeader.css";
-import ProfileImage from "../Header/ProfileImage";
 import { RiDashboardLine, RiFileUserLine } from "react-icons/ri";
 import { NavLink, Link } from "react-router-dom";
 import { GoClippy } from "react-icons/go";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import MenuDropdown from "./MenuDropdown";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowDown, MdArrowDropDown } from "react-icons/md";
 import { connect } from "react-redux";
 import { fetchNewSellers } from "../../redux/actions";
 import ScreenLoader from "../Pages/ScreenLoader";
+import AdminProfile from "./AdminProfile";
+import { IconContext } from "react-icons";
 
 class AdminDashboardSecondaryHeader extends React.Component {
   state = {
@@ -170,7 +171,25 @@ class AdminDashboardSecondaryHeader extends React.Component {
             </NavLink>
           </li>
         </ul>
-        <ProfileImage />
+        {/* <AdminProfile  /> */}
+        <div id="admin-profile-section">
+          <div className="d-flex align-items-center">
+            <AdminProfile id="admin-secondary-header-profile-image" />
+            <IconContext.Provider
+              value={{ className: "admin-secondary-header-dropdown-icon" }}
+            >
+              <MdArrowDropDown />
+            </IconContext.Provider>
+          </div>
+          <div className="admin-logout-section">
+            <p
+              className="p-2"
+              onClick={() => (window.location.href = "/api/logout")}
+            >
+              Logout
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
