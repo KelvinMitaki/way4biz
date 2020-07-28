d764adcf57c42861f4026effea8d1d42d369cd54
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-07-29 01:03:21 +0300

    Added seller setting content

--- client/src/components/Seller/SellerSettings.js
+++ client/src/components/Seller/SellerSettings.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "./SellerSettings.css";
-
 import SellerDashBoardMenu from "./SellerDashBoardMenu";
 import SellerDashBoardHeader from "./SellerDashBoardHeader";
 
@@ -14,7 +13,41 @@ class SellerSettings extends React.Component {
           <div className="col-lg-3">
             <SellerDashBoardMenu />
           </div>
-          <div className="col-lg-9 mx-auto py-3 box-container"></div>
+          <div className="col-lg-9 mx-auto py-3 box-container">
+            {/* <h3 style={{ textAlign: "center" }} className="my-2">
+              Settings
+            </h3> */}
+            <div className="container">
+              <form className="form-group store-settings-form">
+                <h3
+                  style={{ textAlign: "center", textDecoration: "underline" }}
+                >
+                  Personal Details
+                </h3>
+                <label htmlFor="seller-name">Name</label>
+                <input className="form-control" id="seller-name" type="text" />
+                <label htmlFor="phone">Phone</label>
+                <input className="form-control" id="phone" type="text" />
+                <label htmlFor="email">Email</label>
+                <input className="form-control" id="email" type="text" />
+                <h3
+                  style={{ textAlign: "center", textDecoration: "underline" }}
+                >
+                  Store Details
+                </h3>
+                <label htmlFor="store-name">Store Name</label>
+                <input className="form-control" id="store-name" type="text" />
+                <label htmlFor="store-description">Description</label>
+                <textarea
+                  className="form-control"
+                  id="store-description"
+                  type="text"
+                  col
+                ></textarea>
+              </form>
+              <button className="btn store-settings-btn mt-3">Update</button>
+            </div>
+          </div>
         </div>
       </div>
     );
