b71c2590fbb6eebda24a64bd4fc4eab7a6c8fa6f
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-28 17:19:16 +0300

    populated the front end with store data

--- client/src/components/Store/Store.js
+++ client/src/components/Store/Store.js
@@ -10,12 +10,14 @@ import Image from "../Market/Image";
 import HeroCategories from "../Hero/HeroCategories";
 import { fetchStoreProducts } from "../../redux/actions";
 import { connect } from "react-redux";
+import ScreenLoader from "../Pages/ScreenLoader";
 
 class Store extends Component {
   componentDidMount() {
     this.props.fetchStoreProducts(this.props.match.params.sellerId);
   }
   render() {
+    if (!this.props.storeProducts) return <ScreenLoader />;
     return (
       <div>
         {/* if buyer or seller display this header if admin display admin header */}
@@ -31,117 +33,61 @@ class Store extends Component {
                 <h2 style={{ textAlign: "center" }}>Bata Stores</h2>
               </div>
               <div className="products-section">
-                <div className="product">
-                  <Link to="/" title="Helloo World" className="product-link">
-                    <Image image="/1.jpg" alt="test-img" />
-                    <div>
-                      <p
-                        className="product-name"
-                        style={{ padding: "0px 10px" }}
+                {this.props.storeProducts.length !== 0 &&
+                  this.props.storeProducts.map(pro => (
+                    <div className="product">
+                      <Link
+                        to={`/product/${pro._id}`}
+                        title="Helloo World"
+                        className="product-link"
                       >
-                        Great Beer
-                      </p>
-                      <p
+                        <Image
+                          height="200vh"
+                          width="150vw"
+                          image={
+                            pro.imageUrl[0].includes("http")
+                              ? pro.imageUrl[0]
+                              : `https://e-commerce-gig.s3.eu-west-2.amazonaws.com/${pro.imageUrl[0]} `
+                          }
+                          alt={pro.name}
+                        />
+                        <div>
+                          <p
+                            className="product-name"
+                            style={{ padding: "0px 10px" }}
+                          >
+                            {pro.name}
+                          </p>
+                          <p
+                            style={{
+                              fontWeight: "bolder",
+                              padding: "0px 10px"
+                            }}
+                            className="price"
+                          >
+                            Ksh.{pro.price.toLocaleString()}
+                          </p>
+                        </div>
+                      </Link>
+                      {pro.freeShipping && (
+                        <div style={{ height: "10px", padding: "0px 10px" }}>
+                          <p className="lead" style={{ fontSize: "smaller" }}>
+                            Free Shipping
+                          </p>
+                        </div>
+                      )}
+                      <div
                         style={{
-                          fontWeight: "bolder",
+                          display: "flex",
+                          justifyContent: "space-between",
                           padding: "0px 10px"
                         }}
-                        className="price"
+                        className="mb-2"
                       >
-                        Ksh.1500
-                      </p>
+                        {/* <Heart /> */}
+                      </div>
                     </div>
-                  </Link>
-                  <div style={{ height: "10px", padding: "0px 10px" }}>
-                    <p className="lead" style={{ fontSize: "smaller" }}>
-                      Free Shipping
-                    </p>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "0px 10px"
-                    }}
-                    className="mb-2"
-                  >
-                    {/* <Heart /> */}
-                  </div>
-                </div>
-                <div className="product">
-                  <Link to="/" title="Helloo World" className="product-link">
-                    <Image image="/1.jpg" alt="test-img" />
-                    <div>
-                      <p
-                        className="product-name"
-                        style={{ padding: "0px 10px" }}
-                      >
-                        Great Beer
-                      </p>
-                      <p
-                        style={{
-                          fontWeight: "bolder",
-                          padding: "0px 10px"
-                        }}
-                        className="price"
-                      >
-                        Ksh.1500
-                      </p>
-                    </div>
-                  </Link>
-                  <div style={{ height: "10px", padding: "0px 10px" }}>
-                    <p className="lead" style={{ fontSize: "smaller" }}>
-                      Free Shipping
-                    </p>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "0px 10px"
-                    }}
-                    className="mb-2"
-                  >
-                    {/* <Heart /> */}
-                  </div>
-                </div>
-                <div className="product">
-                  <Link to="/" title="Helloo World" className="product-link">
-                    <Image image="/1.jpg" alt="test-img" />
-                    <div>
-                      <p
-                        className="product-name"
-                        style={{ padding: "0px 10px" }}
-                      >
-                        Great Beer
-                      </p>
-                      <p
-                        style={{
-                          fontWeight: "bolder",
-                          padding: "0px 10px"
-                        }}
-                        className="price"
-                      >
-                        Ksh.1500
-                      </p>
-                    </div>
-                  </Link>
-                  <div style={{ height: "10px", padding: "0px 10px" }}>
-                    <p className="lead" style={{ fontSize: "smaller" }}>
-                      Free Shipping
-                    </p>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "0px 10px"
-                    }}
-                    className="mb-2"
-                  >
-                    {/* <Heart /> */}
-                  </div>
-                </div>
+                  ))}
               </div>
             </div>
           </div>
