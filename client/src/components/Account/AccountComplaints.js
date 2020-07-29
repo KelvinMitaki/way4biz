f4dc5f5c8a7d9cc2b188d8a33b0f691c3fc0fe91
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-30 01:41:47 +0300

    populated complaints

--- client/src/components/Account/AccountComplaints.js
+++ client/src/components/Account/AccountComplaints.js
@@ -5,12 +5,19 @@ import Footer from "../Footer/Footer";
 import MiniMenuWrapper from "../MiniMenuWrapper/MiniMenuWrapper";
 import AccountHeader from "../Header/AccountHeader";
 import { Link } from "react-router-dom";
+import { fetchBuyerComplaints } from "../../redux/actions";
+import { connect } from "react-redux";
+import ScreenLoader from "../Pages/ScreenLoader";
 // import { IconContext } from "react-icons";
 // import { BsArrowLeft } from "react-icons/bs";
 // import ScreenLoader from "../Pages/ScreenLoader";
 
 class AccountComplaints extends React.Component {
+  componentDidMount() {
+    this.props.fetchBuyerComplaints();
+  }
   render() {
+    if (!this.props.buyerComplaints) return <ScreenLoader />;
     return (
       <div className="main">
         <div className="content">
@@ -26,80 +33,29 @@ class AccountComplaints extends React.Component {
                 </div>
                 <div className="container mt-3">
                   {/* mapping here */}
-                  <div className="box-container account-complain">
-                    <p>
-                      The quick brown fox jumped over the lazy dog. The quick
-                      brown fox jumped over the lazy dog. The quick brown fox
-                      jumped over the lazy dog. The quick brown fox jumped over
-                      the lazy dog. The quick brown fox jumped over the lazy
-                      dog. The quick brown fox jumped over the lazy dog.
-                    </p>
-                    <p className="d-flex justify-content-end">
-                      <Link to="/complaint" className="complaint-more-link">
-                        More
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="box-container account-complain">
-                    <p>
-                      The quick brown fox jumped over the lazy dog. The quick
-                      brown fox jumped over the lazy dog. The quick brown fox
-                      jumped over the lazy dog. The quick brown fox jumped over
-                      the lazy dog. The quick brown fox jumped over the lazy
-                      dog. The quick brown fox jumped over the lazy dog.
-                    </p>
-                    <p className="d-flex justify-content-end">
-                      <Link to="/complaint" className="complaint-more-link">
-                        More
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="box-container account-complain">
-                    <p>
-                      The quick brown fox jumped over the lazy dog. The quick
-                      brown fox jumped over the lazy dog. The quick brown fox
-                      jumped over the lazy dog. The quick brown fox jumped over
-                      the lazy dog. The quick brown fox jumped over the lazy
-                      dog. The quick brown fox jumped over the lazy dog.
-                    </p>
-                    <p className="d-flex justify-content-end">
-                      <Link to="/complaint" className="complaint-more-link">
-                        More
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="box-container account-complain">
-                    <p>
-                      The quick brown fox jumped over the lazy dog. The quick
-                      brown fox jumped over the lazy dog. The quick brown fox
-                      jumped over the lazy dog. The quick brown fox jumped over
-                      the lazy dog. The quick brown fox jumped over the lazy
-                      dog. The quick brown fox jumped over the lazy dog.
-                    </p>
-                    <p className="d-flex justify-content-end">
-                      <Link to="/complaint" className="complaint-more-link">
-                        More
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="box-container account-complain">
-                    <p>
-                      The quick brown fox jumped over the lazy dog. The quick
-                      brown fox jumped over the lazy dog. The quick brown fox
-                      jumped over the lazy dog. The quick brown fox jumped over
-                      the lazy dog. The quick brown fox jumped over the lazy
-                      dog. The quick brown fox jumped over the lazy dog.
-                    </p>
-                    <p className="d-flex justify-content-end">
-                      <Link to="/complaint" className="complaint-more-link">
-                        More
-                      </Link>
-                    </p>
-                  </div>
-                  {/* no fucking complaints */}
-                  <div className="no-buyer-complaints">
-                    <h4>No complaints filed yet.</h4>
-                  </div>
+                  {this.props.buyerComplaints.length !== 0 &&
+                    this.props.buyerComplaints.map(comp => (
+                      <div
+                        key={comp._id}
+                        className="box-container account-complain"
+                      >
+                        <p>{comp.body}</p>
+                        <p className="d-flex justify-content-end">
+                          <Link
+                            to={`/complaint/${comp._id}`}
+                            className="complaint-more-link"
+                          >
+                            More
+                          </Link>
+                        </p>
+                      </div>
+                    ))}
+
+                  {this.props.buyerComplaints.length === 0 && (
+                    <div className="no-buyer-complaints">
+                      <h4>No complaints filed yet.</h4>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -111,5 +67,11 @@ class AccountComplaints extends React.Component {
     );
   }
 }
-
-export default AccountComplaints;
+const mapStateToProps = state => {
+  return {
+    buyerComplaints: state.product.buyerComplaints
+  };
+};
+export default connect(mapStateToProps, { fetchBuyerComplaints })(
+  AccountComplaints
+);
