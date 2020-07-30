e51486dfd3fab2ed80710c0020283ea881ca1f9d
kelvinmitaki <mitakikelvin1@gmail.com>
2020-07-30 21:49:48 +0300

    added route to update seller

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -430,6 +430,42 @@ export const registerSeller = credentials => async (dispatch, getState) => {
     dispatch({ type: LOADING_STOP });
   }
 };
+export const updateSeller = credentials => async (dispatch, getState) => {
+  try {
+    dispatch({ type: LOADING_START });
+
+    const res = await axios.post("/api/seller/update/info", credentials);
+
+    dispatch({ type: REGISTER_SELLER, payload: res.data });
+    dispatch({ type: LOADING_STOP });
+  } catch (error) {
+    if (error.response.data.email) {
+      getState().form.SellerRegister.values.email = "";
+      dispatch({
+        type: REGISTER_SELLER_FAILED,
+        payload: error.response.data.email
+      });
+      dispatch({ type: LOADING_STOP });
+      return;
+    }
+    if (Object.keys(error.response.data.keyPattern)[0] === "phoneNumber") {
+      dispatch({
+        type: REGISTER_SELLER_FAILED,
+        payload: "That phone number already exists"
+      });
+      dispatch({ type: LOADING_STOP });
+      return;
+    }
+    getState().form.SellerRegister.values[
+      Object.keys(error.response.data.keyPattern)[0]
+    ] = "";
+    dispatch({
+      type: REGISTER_SELLER_FAILED,
+      payload: "That store name already exists"
+    });
+    dispatch({ type: LOADING_STOP });
+  }
+};
 
 export const fetchSeller = () => async dispatch => {
   try {
