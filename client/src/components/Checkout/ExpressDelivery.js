d4690e2739d267f8dd222ae6199cd4837afa8fed
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-05 19:58:22 +0300

    Styled express delivery component

--- client/src/components/Checkout/ExpressDelivery.js
+++ client/src/components/Checkout/ExpressDelivery.js
@@ -14,31 +14,35 @@ class ExpressDelivery extends React.Component {
       <div className="main">
         <div className="content">
           <Header />
-          <div className="d-flex align-items-center">
-            <div style={{ flex: "1" }}>
-              <IconContext.Provider
-                value={{ className: "arrow-icon ml-3 my-2" }}
-              >
-                <div className="d-flex align-items-center">
-                  {/* goback() */}
-                  <Link to="/">
-                    <BsArrowLeft />
-                  </Link>
-                </div>
-              </IconContext.Provider>
-            </div>
+          <div className="container mt-4 box-container py-3">
+            <div className="d-flex align-items-center">
+              <div style={{ flex: "1" }}>
+                <IconContext.Provider
+                  value={{ className: "arrow-icon ml-3 my-2" }}
+                >
+                  <div className="d-flex align-items-center">
+                    {/* goback() */}
+                    <Link to="/">
+                      <BsArrowLeft />
+                    </Link>
+                  </div>
+                </IconContext.Provider>
+              </div>
 
-            <h3 className="ml-1" style={{ flex: "2" }}>
-              Express delivery
-            </h3>
-          </div>{" "}
-          <h6>
-            this kind of shipping takes an average of 1-2 days. It is expensive
-            than normal.The price is calculated based on distance.
-          </h6>
-          <p>
-            <Link to="/normal-delivery">About normal shipping</Link>
-          </p>
+              <h3 className="ml-1" style={{ flex: "2" }}>
+                Express delivery
+              </h3>
+            </div>{" "}
+            <h6 className="my-3">
+              This kind of shipping takes an average of 1-2 days. It is
+              expensive than normal.The price is calculated based on distance.
+            </h6>
+            <p>
+              <Link to="/normal-delivery" style={{ color: "#f76b1a" }}>
+                About normal shipping
+              </Link>
+            </p>
+          </div>
         </div>
         <Footer />
         <MiniMenuWrapper />
