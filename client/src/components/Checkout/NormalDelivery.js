06d47dfedc2263118c808aaa3a13feb990008636
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-05 19:14:53 +0300

    Addded content to normal delivery component

--- client/src/components/Checkout/NormalDelivery.js
+++ client/src/components/Checkout/NormalDelivery.js
@@ -4,6 +4,7 @@ import "./NormalDelivery.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MiniMenuWrapper from "../MiniMenuWrapper/MiniMenuWrapper";
+import { Link } from "react-router-dom";
 
 class NormalDelivery extends React.Component {
   render() {
@@ -11,7 +12,31 @@ class NormalDelivery extends React.Component {
       <div className="main">
         <div className="content">
           <Header />
-          <div className="container"></div>
+          <div className="d-flex align-items-center">
+            <div style={{ flex: "1" }}>
+              <IconContext.Provider
+                value={{ className: "arrow-icon ml-3 my-2" }}
+              >
+                <div className="d-flex align-items-center">
+                  {/* goback() */}
+                  <Link to="/orders">
+                    <BsArrowLeft />
+                  </Link>
+                </div>
+              </IconContext.Provider>
+            </div>
+
+            <h3 className="ml-1" style={{ flex: "2" }}>
+              Normal delivery
+            </h3>
+          </div>{" "}
+          <h6>
+            this kind of shipping takes an average of 3-7 days. It is cheaper
+            than express.The price is calculated based on distance.
+          </h6>
+          <p>
+            <Link to="/">About express shipping</Link>
+          </p>
         </div>
         <Footer />
         <MiniMenuWrapper />
