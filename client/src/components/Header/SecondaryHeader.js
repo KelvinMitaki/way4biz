7ca537761f7cb5728d5d0e51215ad2389d757efd
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-07 01:22:13 +0300

    Styled secondary header

--- client/src/components/Header/SecondaryHeader.js
+++ client/src/components/Header/SecondaryHeader.js
@@ -8,17 +8,23 @@ class SecondaryHeader extends React.Component {
     return (
       <div className="secondary-header d-flex primary-background">
         <div id="large-screen-secondary-header" className="d-flex">
-          {!this.props.isSignedIn && (
-            <Link className="mx-4 secondary-header-link" to="/seller/register">
-              Sell with us
+          <div id="secondary-header-side" style={{ flex: "1" }}></div>
+          <div className="d-flex" style={{ flex: "7" }}>
+            {!this.props.isSignedIn && (
+              <Link
+                className="mx-4 secondary-header-link"
+                to="/seller/register"
+              >
+                Sell with us
+              </Link>
+            )}
+            <Link className="mx-4 secondary-header-link" to="/logistics">
+              Logistics Services
             </Link>
-          )}
-          <Link className="mx-4 secondary-header-link" to="/logistics">
-            Logistics Services
-          </Link>
-          <Link className="mx-4 secondary-header-link" to="/">
-            Help
-          </Link>
+            <Link className="mx-4 secondary-header-link" to="/">
+              Help center
+            </Link>
+          </div>
         </div>
         <div id="small-screen-secondary-header">
           <div className="small-screen-secondary-header-items">
@@ -41,14 +47,14 @@ class SecondaryHeader extends React.Component {
               className="mx-4 small-screen-secondary-header-link"
               to="/about-us"
             >
-              Help
+              Help center
             </Link>
-            <Link
+            {/* <Link
               className="mx-4 small-screen-secondary-header-link"
               to="/contact-us"
             >
               Contact Us
-            </Link>
+            </Link> */}
           </div>
         </div>
       </div>
