01a1061d03c6baa7c1f125081bf284462781f966
kelvinmitaki <mitakikelvin1@gmail.com>
2020-08-06 19:44:12 +0300

    fixed status on admin order

--- client/src/components/Admin/AdminDashBoardOrder.js
+++ client/src/components/Admin/AdminDashBoardOrder.js
@@ -67,11 +67,16 @@ class AdminDashBoardOrder extends React.Component {
                     <div className="col-md-6">
                       <p>
                         <strong className="mr-2">Status:</strong>
-                        {this.props.adminOrder["0"].delivered ? (
-                          <span> Delivered</span>
-                        ) : (
-                          <span> Pending</span>
-                        )}
+                        {(this.props.adminOrder["0"].cancelled && (
+                          <span> Cancelled</span>
+                        )) ||
+                          (this.props.adminOrder["0"].delivered && (
+                            <span> Delivered</span>
+                          )) ||
+                          (this.props.adminOrder["0"].paid &&
+                            !this.props.adminOrder["0"].delivered && (
+                              <span> Pending</span>
+                            ))}
                       </p>
                     </div>
                     <div className="col-md-6">
@@ -107,7 +112,7 @@ class AdminDashBoardOrder extends React.Component {
                       alignItems: "center",
                       justifyContent: "center",
                       borderTop: "1px solid #d4d4d4",
-                      padding: "20px",
+                      padding: "20px"
                     }}
                   >
                     <Link
