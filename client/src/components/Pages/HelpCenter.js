8bf26a113d281d3bb748da59ebf3826f28f04c86
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-07 14:09:38 +0300

    Added help center header

--- client/src/components/Pages/HelpCenter.js
+++ client/src/components/Pages/HelpCenter.js
@@ -11,8 +11,10 @@ class HelpCenter extends React.Component {
       <div className="main">
         <div className="content">
           <Header />
-          <div className="container-fluid p-0">
-            <div className="help-center-hero"></div>
+          <div className="container-fluid p-0 m-0">
+            <div className="help-center-hero">
+              <h1 id="help-center-header">Looking for something?</h1>
+            </div>
           </div>
         </div>
         <Footer />
