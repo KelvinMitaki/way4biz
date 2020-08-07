8d0947eb22102f02eb37d94f5ea2faeb961dc31d
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-07 14:32:23 +0300

    Styled help center input

--- client/src/components/Pages/HelpCenter.js
+++ client/src/components/Pages/HelpCenter.js
@@ -4,6 +4,7 @@ import "./HelpCenter.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MiniMenuWrapper from "../MiniMenuWrapper/MiniMenuWrapper";
+import { AiOutlineSearch } from "react-icons/ai";
 
 class HelpCenter extends React.Component {
   render() {
@@ -13,7 +14,24 @@ class HelpCenter extends React.Component {
           <Header />
           <div className="container-fluid p-0 m-0">
             <div className="help-center-hero">
-              <h1 id="help-center-header">Looking for something?</h1>
+              <h1 id="help-center-header">How can we help?</h1>
+
+              <div className="input-group mt-3" id="help-center-input">
+                <div className="input-group-prepend">
+                  <div
+                    style={{
+                      backgroundColor: "#fff",
+                      width: "40px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <AiOutlineSearch style={{ fontSize: "20px" }} />
+                  </div>
+                </div>
+                <input className="form-control" placeholder="Search..." />
+              </div>
             </div>
           </div>
         </div>
