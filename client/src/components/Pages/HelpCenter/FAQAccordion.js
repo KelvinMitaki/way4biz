3475f3a5c82efa4d1ba07bc9fa447df83524b20f
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-10 23:17:14 +0300

    Added panel component

--- client/src/components/Pages/HelpCenter/FAQAccordion.js
+++ client/src/components/Pages/HelpCenter/FAQAccordion.js
@@ -1,39 +1,13 @@
 import React from "react";
 import { AiOutlineMinusCircle } from "react-icons/ai";
 import { BsPlusCircle } from "react-icons/bs";
+import "./FAQAccordion.css";
 
 class FAQAccordion extends React.Component {
   state = { open: false };
 
-  handleOpen = (e) => {
-    this.setState({
-      open: true,
-    });
-  };
-
-  handleClose = (e) => {
-    this.setState({
-      open: false,
-    });
-  };
   render() {
-    return (
-      <div>
-        {!this.state.open ? (
-          <div>
-            <p onClick={this.handleOpen}>
-              <BsPlusCircle /> How to sell on way4biz?
-            </p>
-          </div>
-        ) : (
-          <div>
-            <p onClick={this.handleClose}>
-              <AiOutlineMinusCircle /> How to sell on way4biz?
-            </p>
-          </div>
-        )}
-      </div>
-    );
+    return <div></div>;
   }
 }
 
