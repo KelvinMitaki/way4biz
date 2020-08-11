8230a5cda529e579a305c55c4c758a66fb7946dc
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-11 23:52:11 +0300

    Added seller points tabs

--- client/src/components/Seller/SellerPoints.js
+++ client/src/components/Seller/SellerPoints.js
@@ -1,10 +1,30 @@
 import React from "react";
-
+import Tabs from "react-responsive-tabs";
 import "./SellerPoints.css";
 import SellerDashBoardHeader from "./SellerDashBoardHeader";
 import SellerDashBoardMenu from "./SellerDashBoardMenu";
 
 class SellerPoints extends React.Component {
+  getTabs() {
+    const data = [
+      {
+        name: "Earn Points",
+        content: "The quick brown fox jumped over the lazy dog",
+      },
+      {
+        name: "Redeem Points",
+        content: "The quick brown fox jumped over the lazy dog",
+      },
+    ];
+
+    return data.map((d, index) => ({
+      title: d.name,
+      getContent: () => d.content,
+      key: index,
+      tabClassName: "points-tab",
+      panelClassName: "seller-db-panel",
+    }));
+  }
   render() {
     return (
       <div className="container-fluid dashboard-wrapper">
@@ -14,7 +34,19 @@ class SellerPoints extends React.Component {
             <SellerDashBoardMenu />
           </div>
           <div className="col-lg-9 mt-5">
-            <div className="container seller-dashboard-wrapper m-0"></div>
+            <div className="container seller-dashboard-wrapper m-0">
+              <div className="row">
+                <div className="col-10 col-md-8 mx-auto my-5">
+                  <Tabs
+                    items={this.getTabs()}
+                    transformWidth={100}
+                    transform={true}
+                    showMoreLabel={"More..."}
+                    showInkBar={true}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
