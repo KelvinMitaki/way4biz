e15159a1dd7fd9d1aa3e3788c586a4d220e18354
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-11 23:28:54 +0300

    Added the seller points component

---
+++ client/src/components/Seller/SellerPoints.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./SellerPoints.css";
+
+class SellerPoints extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid dashboard-wrapper">
+        <SellerDashBoardHeader />
+        <div className="row no-gutters">
+          <div className="col-lg-3">
+            <SellerDashBoardMenu />
+          </div>
+          <div className="col-lg-9 mt-5">
+            <div className="container seller-dashboard-wrapper m-0"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SellerPoints;
