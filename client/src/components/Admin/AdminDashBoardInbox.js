aa319ab2626501e8a40b8a38051a7d9d8bb6e281
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-11 22:10:55 +0300

    Added inbox mail content

--- client/src/components/Admin/AdminDashBoardInbox.js
+++ client/src/components/Admin/AdminDashBoardInbox.js
@@ -10,11 +10,36 @@ class AdminDashBoardInbox extends React.Component {
       <div className="container-fluid dashboard-wrapper">
         <MobileLogo />
         <AdminDashBoardHeader />
-        <div className="container box-container">
-          <h3>Inbox</h3>
-          <div className="box-container">
-            <h6>Sender:</h6>
-            <h6>Subject</h6>
+        <div className="container box-container mt-3">
+          <h3 style={{ textAlign: "center" }} className="my-2">
+            Inbox
+          </h3>
+          <div className="box-container p-2">
+            <div className="row">
+              <div className="col-md-4">
+                <h6>
+                  <strong>First Name:</strong>Frank
+                </h6>
+              </div>
+              <div className="col-md-4">
+                <h6>
+                  <strong>Last Name:</strong>Zagarino
+                </h6>
+              </div>
+              <div className="col-md-4">
+                <h6>
+                  <strong>Email:</strong>franc@gmail.com
+                </h6>
+              </div>
+            </div>
+            <h6 className="my-2">
+              <strong>Subject:</strong>Order Delay
+            </h6>
+            <p className="my-2">
+              The quick brown fox jumped over the lazy dog. The quick brown fox
+              jumped over the lazy dog. The quick brown fox jumped over the lazy
+              dog.The quick brown fox jumped over the lazy dog.
+            </p>
           </div>
         </div>
       </div>
