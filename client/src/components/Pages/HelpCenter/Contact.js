bd1221872fb101ffd793b40e07b5a796f5043e6b
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-08-12 19:52:28 +0300

    Added contact form

--- client/src/components/Pages/HelpCenter/Contact.js
+++ client/src/components/Pages/HelpCenter/Contact.js
@@ -15,8 +15,53 @@ class Contact extends React.Component {
             className="container-fluid p-0 m-0"
             style={{ backgroundColor: "#fff" }}
           >
-            <h3 style={{ textAlign: "center" }}>Contact Us</h3>
-            <form className="form-group"></form>
+            <div className="container">
+              <h3 style={{ textAlign: "center" }}>Contact Us</h3>
+              <form className="form-group mt-4">
+                <label htmlFor="contact-reason">Reason</label>
+                <select className="form-control" id="contact-reason">
+                  <option value="suggestion">Suggestion</option>
+                  <option value="feedback">Feedback</option>
+                </select>
+                <div className="row">
+                  <div className="col-md-6">
+                    <label htmlFor="contact-first-name">First Name</label>
+                    <input
+                      className="form-control"
+                      id="contact-first-name"
+                      type="text"
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <label htmlFor="contact-last-name">Last Name</label>
+                    <input
+                      className="form-control"
+                      id="contact-last-name"
+                      type="text"
+                    />
+                  </div>
+                </div>
+                <label htmlFor="contact-email">Email</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="contact-email"
+                />
+                <label htmlFor="contact-subject">Subject</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="contact-subject"
+                />
+                <label htmlFor="contact-message">Message</label>
+                <textarea
+                  className="form-control"
+                  id="contact-message"
+                ></textarea>
+
+                <button className="btn btn-block contact-btn my-3">Send</button>
+              </form>
+            </div>
           </div>
         </div>
         <Footer />
