903cf7c5776fa376ca65c71bbaa8374c69f277c7
kelvinmitaki <mitakikelvin1@gmail.com>
2020-08-13 00:20:17 +0300

    fixed earn points

--- client/src/components/Seller/EarnPoints.js
+++ client/src/components/Seller/EarnPoints.js
@@ -6,17 +6,15 @@ import EarnPointsInput from "./EarnPointsInput";
 import { connect } from "react-redux";
 import validator from "validator";
 let email;
-let phoneNumber;
 class EarnPoints extends React.Component {
   render() {
     email = this.props.email;
-    phoneNumber = this.props.phoneNumber;
     return (
       <div className="container py-4" style={{ backgroundColor: "#fff" }}>
         <h6>
-          You currently have 1000 points. To earn more points refer many sellers
-          to sell on our platform. The more points you have, the higher the
-          chances of winnig a brand new suzuki porsche.
+          You currently have {this.props.points} points. To earn more points
+          refer many sellers to sell on our platform. The more points you have,
+          the higher the chances of winnig a brand new suzuki porsche.
         </h6>
 
         <h6 className="my-2">
@@ -53,32 +51,19 @@ const validate = formValues => {
   if (!formValues.points) {
     errors.points = "Please fill in this field";
   }
-  if (
-    formValues.points &&
-    !validator.isEmail(formValues.points.trim()) &&
-    formValues.points &&
-    !validator.isNumeric(formValues.points)
-  ) {
+  if (formValues.points && !validator.isEmail(formValues.points.trim())) {
     errors.points = "Please enter a valid email or phone number";
   }
   if (formValues.points && formValues.points === email) {
     errors.points = "Invalid email";
   }
-  if (formValues.points && formValues.points === phoneNumber) {
-    errors.points = "Invalid Phone Number";
-  }
-  if (
-    formValues.points &&
-    validator.isNumeric(formValues.points) &&
-    formValues.points.length !== 9
-  ) {
-    errors.points = "Please enter a 9 digit phone number";
-  }
+
   return errors;
 };
 const mapStateToProps = state => {
   return {
     email: state.auth.user.email,
+    points: state.auth.user.points,
     phoneNumber: state.auth.user.phoneNumber
   };
 };
