98f2db6cf574be9a68053fb2e791fe269c289eb1
kelvinmitaki <mitakikelvin1@gmail.com>
2020-08-14 23:03:54 +0300

    transfered complaint loading to the admin

--- client/src/redux/reducers/adminReducer.js
+++ client/src/redux/reducers/adminReducer.js
@@ -1,12 +1,20 @@
 import {
   FETCH_ADMIN_INBOX,
   FETCH_ADMIN_INBOX_START,
-  FETCH_ADMIN_INBOX_STOP
+  FETCH_ADMIN_INBOX_STOP,
+  FETCH_COMPLAINT_START,
+  FETCH_COMPLAINT_STOP,
+  NEW_COMPLAINT_START,
+  NEW_COMPLAINT_STOP
 } from "../actions/types";
 
 const INITIAL_STATE = {
   inbox: null,
-  inboxLoading: false
+  inboxLoading: false,
+  complaintLoading: false,
+  complaintsCount: null,
+  complaints: null,
+  complaint: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -17,6 +25,14 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, inboxLoading: true };
     case FETCH_ADMIN_INBOX_STOP:
       return { ...state, inboxLoading: false };
+    case FETCH_COMPLAINT_START:
+      return { ...state, complaintLoading: true };
+    case FETCH_COMPLAINT_STOP:
+      return { ...state, complaintLoading: false };
+    case NEW_COMPLAINT_START:
+      return { ...state, complaintLoading: true };
+    case NEW_COMPLAINT_STOP:
+      return { ...state, complaintLoading: false };
     default:
       return state;
   }
