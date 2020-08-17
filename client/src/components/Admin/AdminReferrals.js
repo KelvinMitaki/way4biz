161f73f04a55edcb9b149da3b923ac63ad7ea079
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-17 20:41:33 +0300

    Added admin referrals markup

--- client/src/components/Admin/AdminReferrals.js
+++ client/src/components/Admin/AdminReferrals.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 import "./AdminReferrals.css";
+import MobileLogo from "../Header/MobileLogo";
+import AdminDashBoardHeader from "./AdminDashBoardHeader";
+import AdminDashboardSecondaryHeader from "./AdminDashboardSecondaryHeader";
 
 class AdminReferrals extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container-fluid dashboard-wrapper">
+        <MobileLogo />
+        <AdminDashBoardHeader />
+        <div className="container-fluid p-0">
+          <AdminDashboardSecondaryHeader />
+          <div className="container box-container mt-3">
+            <h3 style={{ textAlign: "center" }} className="my-2">
+              Referrals
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
