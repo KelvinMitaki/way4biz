f3b0729b56c9469ac858667b1e5c0e8b8519ea4e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-17 14:25:13 +0300

    added timestamp to user schema

--- models/User.js
+++ models/User.js
@@ -1,49 +1,52 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String
+    },
+    phoneNumber: {
+      type: Number
+    },
+    city: {
+      type: String
+    },
+    town: {
+      type: String
+    },
+    address: {
+      type: String
+    },
+    isSeller: {
+      type: Boolean,
+      default: false
+    },
+    verified: {
+      type: Boolean,
+      default: false
+    },
+    googleId: {
+      type: String
+    },
+    cart: {
+      type: mongoose.Types.ObjectId,
+      ref: "Cart"
+    }
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String
-  },
-  phoneNumber: {
-    type: Number
-  },
-  city: {
-    type: String
-  },
-  town: {
-    type: String
-  },
-  address: {
-    type: String
-  },
-  isSeller: {
-    type: Boolean,
-    default: false
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  googleId: {
-    type: String
-  },
-  cart: {
-    type: mongoose.Types.ObjectId,
-    ref: "Cart"
-  }
-});
+  { timestamps: true }
+);
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
