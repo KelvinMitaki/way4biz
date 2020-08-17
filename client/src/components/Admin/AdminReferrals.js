b6784ca86f8e5994b0fe55b95f27e55b31b04cb4
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-17 20:39:46 +0300

    Added admin referrals component

---
+++ client/src/components/Admin/AdminReferrals.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./AdminReferrals.css";
+
+class AdminReferrals extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AdminReferrals;
