1d6db0ceb0349316c0c345c40b65ac8b2f9b6afb
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-18 20:32:33 +0300

    Changed footer terms and conditons routing

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -62,7 +62,7 @@ class Footer extends React.Component {
                   )}
                 </p>
                 <p>
-                  <Link to="/how-to-sell">Terms and Conditions</Link>
+                  <Link to="/terms">Terms and Conditions</Link>
                 </p>
               </div>
             </div>
@@ -88,10 +88,10 @@ class Footer extends React.Component {
     );
   }
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     user: state.auth.user,
-    isSignedIn: state.auth.isSignedIn
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 export default connect(mapStateToProps)(Footer);
