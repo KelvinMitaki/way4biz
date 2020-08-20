a767b3e0d55d964e71014cbb006c52a02279cdf2
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-20 18:28:25 +0300

    Added rules about your content terms and conditions

--- client/src/components/Pages/HelpCenter/TermsConditions.js
+++ client/src/components/Pages/HelpCenter/TermsConditions.js
@@ -47,6 +47,33 @@ class TermsConditions extends React.Component {
 
               <h3>Payments</h3>
               <p>Any buyer must make payments due under these general terms and conditions in accordance with the Payments Information and Guidelines on the platform.</p>
+
+              <h3>Rules about your content</h3>
+              <h6>In these general terms and conditions, "your content" means:</h6>
+              <ul>
+                <li><p>All works and materials (including without limitation text, graphics, images, audio material, video material, audio-visual material, scripts, software and files) that you submit to us or our platform for storage or publication, processing by, or onward transmission; and</p></li>
+                <li><p>All communications on the platform, including product reviews, feedback and comments.</p></li>
+              </ul>
+              <h6>Your content must be appropriate, civil and tasteful, and accord with generally accepted standards of etiquette and behaviour on the internet, and must not:</h6>
+              <ul>
+                <li><p>Be offensive, obscene, indecent, pornographic, lewd, suggestive or sexually explicit;</p></li>
+                <li><p>Depict violence in an explicit, graphic or gratuitous manner; </p></li>
+                <li><p>Be blasphemous, in breach of racial or religious hatred or discrimination legislation;</p></li>
+                <li><p>Be deceptive, fraudulent, threatening, abusive, harassing, anti-social, menacing, hateful, discriminatory or inflammatory;</p></li>
+                <li><p>Cause annoyance, inconvenience or needless anxiety to any person; or</p></li>
+                <li><p>Constitute spam.</p></li>
+              </ul>
+              <h6>Your content must not be illegal or unlawful, infringe any person's legal rights, or be capable of giving rise to legal action against any person (in each case in any jurisdiction and under any applicable law). Your content must not infringe or breach:</h6>
+              <ul>
+                <li><p>Any copyright, moral right, database right, trademark right, design right, right in passing off or other intellectual property right;</p></li>
+                <li><p>Any right of confidence, right of privacy or right under data protection legislation;</p></li>
+                <li><p>Any contractual obligation owed to any person; or</p></li>
+                <li><p>Any court order.</p></li>
+              </ul>
+              <p>You must not use our platform to link to any website or web page consisting of or containing material that would, were it posted on our platform, breach the provisions of these general terms and conditions.</p>
+              <p>The review function on the platform may be used to facilitate buyer reviews on products. You shall not use the review function or any other form of communication to provide inaccurate, inauthentic or fake reviews.</p>
+              <p>We may periodically review your content and we reserve the right to remove any content in our discretion for any reason whatsoever.</p>
+
             </div>
           </div>
         </div>
