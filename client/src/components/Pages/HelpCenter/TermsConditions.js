062a0bf24a16566cf23157e10ca613e41bff2723
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-20 17:42:53 +0300

    Added introduction terms and conditions

--- client/src/components/Pages/HelpCenter/TermsConditions.js
+++ client/src/components/Pages/HelpCenter/TermsConditions.js
@@ -19,6 +19,9 @@ class TermsConditions extends React.Component {
             <div className="terms-content">
               <h3>Introduction</h3>
               <p>Way4Biz is a 2 in 1 company that operates on an e-commerce platform consisting of a web application and mobile applications(Ecommerce platform application and a logistics application) ,supporting logistics and payment infrastructure, for the sale,purchase and delivery of consumer products.</p>
+              <p>These Way4Biz general terms and conditions shall apply to buyers and sellers on the platforms and shall govern the use of the platform and related services.</p>
+              <p>By using our platform, you accept these general terms and conditions in full. If you disagree with anything then you should not use our platform.</p>
+              <p>You can only use our platform to expand your business marketing strategy or in delivering your goods if and only if you agree to this terms and conditions</p>
             </div>
           </div>
         </div>
