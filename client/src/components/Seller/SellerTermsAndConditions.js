62b53d431497ddf9097e16798e14f4c07897d1bb
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-08-21 09:50:47 +0300

    Added seller terms and conditons

--- client/src/components/Seller/SellerTermsAndConditions.js
+++ client/src/components/Seller/SellerTermsAndConditions.js
@@ -21,55 +21,112 @@ class SellerTermsAndConditions extends React.Component {
           >
             Terms and Conditions
           </h3>
-          <p>
-            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum. Why do we use it? It is a long established fact that a reader
-            will be distracted by the readable content of a page when looking at
-            its layout. The point of using Lorem Ipsum is that it has a
-            more-or-less normal distribution of letters, as opposed to using
-            'Content here, content here', making it look like readable English.
-            Many desktop publishing packages and web page editors now use Lorem
-            Ipsum as their default model text, and a search for 'lorem ipsum'
-            will uncover many web sites still in their infancy. Various versions
-            have evolved over the years, sometimes by accident, sometimes on
-            purpose (injected humour and the like). Where does it come from?
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32. The standard chunk of
-            Lorem Ipsum used since the 1500s is reproduced below for those
-            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-            Malorum" by Cicero are also reproduced in their exact original form,
-            accompanied by English versions from the 1914 translation by H.
-            Rackham. Where can I get some? There are many variations of passages
-            of Lorem Ipsum available, but the majority have suffered alteration
-            in some form, by injected humour, or randomised words which don't
-            look even slightly believable. If you are going to use a passage of
-            Lorem Ipsum, you need to be sure there isn't anything embarrassing
-            hidden in the middle of text. All the Lorem Ipsum generators on the
-            Internet tend to repeat predefined chunks as necessary, making this
-            the first true generator on the Internet. It uses a dictionary of
-            over 200 Latin words, combined with a handful of model sentence
-            structures, to generate Lorem Ipsum which looks reasonable. The
-            generated Lorem Ipsum is therefore always free from repetition,
-            injected humour, or non-characteristic words etc.
-          </p>
+          <div className="terms-content">
+            <h3>Introduction</h3>
+            <p>Way4Biz is a 2 in 1 company that operates on an e-commerce platform consisting of a web application and mobile applications(Ecommerce platform application and a logistics application) ,supporting logistics and payment infrastructure, for the sale,purchase and delivery of consumer products.</p>
+            <p>These Way4Biz general terms and conditions shall apply to buyers and sellers on the platforms and shall govern the use of the platform and related services.</p>
+            <p>By using our platform, you accept these general terms and conditions in full. If you disagree with anything then you should not use our platform.</p>
+            <p>You can only use our platform to expand your business marketing strategy or in delivering your goods if and only if you agree to this terms and conditions</p>
+
+            <h3>Registration</h3>
+            <p>You may register as a buyer or seller on our platform. A seller however can act as a buyer but a buyer cannot act as a seller.</p>
+            <p>To register for an account on our platform you need to complete the registration forms as required and submit it.</p>
+            <p>If you register for an account with our platform as a buyer, you will be asked to provide an email address which is unique ,a valid phone number and also a password which should be confidential.</p>
+            <p>. If you register for an account with our platform as a seller, you will be asked to provide an email address which is unique ,a valid phone number , a password which should be confidential ,your storename and also its description.</p>
+            <p>Any activity in your account that may not align with our terms of operation may result to immediate suspension or even deletion of your account.</p>
+
+            <h3>Terms and conditions of sale</h3>
+            <h6>You acknowledge that:</h6>
+            <ul>
+              <li><p>We provide an online platform for sellers to sell and buyers to purchase products;</p></li>
+              <li><p>We accept  sales  and do the delivery on behalf of the sellers to the consumers.</p></li>
+            </ul>
+            <p>The price for a product will be as stated in the relevant product catalogue;</p>
+            <p>The price for the product must include all taxes and comply with applicable laws in force from time to time;</p>
+            <p>Products must be of satisfactory quality, fit and safe for any purpose specified in, and conform in all material respects to, the product catalogue and any other description of the products supplied or made available by the seller to the buyer.</p>
+
+            <h3>Returns and refunds</h3>
+            <p>Returns of products by buyers and acceptance of returned products by sellers shall be managed by us in accordance with the returns page on the platform, as may be amended from time to time. Acceptance of returns shall be in our discretion, subject to compliance with applicable laws of the territory.</p>
+            <p>Returned products shall be accepted and refunds issued by Way4Biz, for and on behalf of the seller.</p>
+            <p>Changes to our returns page or refunds page shall be effective in respect of all purchases made from the date of publication of the change on our website.</p>
+
+            <h3>Payments</h3>
+            <p>Any buyer must make payments due under these general terms and conditions in accordance with the Payments Information and Guidelines on the platform.</p>
+
+            <h3>Rules about your content</h3>
+            <h6>In these general terms and conditions, "your content" means:</h6>
+            <ul>
+              <li><p>All works and materials (including without limitation text, graphics, images, audio material, video material, audio-visual material, scripts, software and files) that you submit to us or our platform for storage or publication, processing by, or onward transmission; and</p></li>
+              <li><p>All communications on the platform, including product reviews, feedback and comments.</p></li>
+            </ul>
+            <h6>Your content must be appropriate, civil and tasteful, and accord with generally accepted standards of etiquette and behaviour on the internet, and must not:</h6>
+            <ul>
+              <li><p>Be offensive, obscene, indecent, pornographic, lewd, suggestive or sexually explicit;</p></li>
+              <li><p>Depict violence in an explicit, graphic or gratuitous manner; </p></li>
+              <li><p>Be blasphemous, in breach of racial or religious hatred or discrimination legislation;</p></li>
+              <li><p>Be deceptive, fraudulent, threatening, abusive, harassing, anti-social, menacing, hateful, discriminatory or inflammatory;</p></li>
+              <li><p>Cause annoyance, inconvenience or needless anxiety to any person; or</p></li>
+              <li><p>Constitute spam.</p></li>
+            </ul>
+            <h6>Your content must not be illegal or unlawful, infringe any person's legal rights, or be capable of giving rise to legal action against any person (in each case in any jurisdiction and under any applicable law). Your content must not infringe or breach:</h6>
+            <ul>
+              <li><p>Any copyright, moral right, database right, trademark right, design right, right in passing off or other intellectual property right;</p></li>
+              <li><p>Any right of confidence, right of privacy or right under data protection legislation;</p></li>
+              <li><p>Any contractual obligation owed to any person; or</p></li>
+              <li><p>Any court order.</p></li>
+            </ul>
+            <p>You must not use our platform to link to any website or web page consisting of or containing material that would, were it posted on our platform, breach the provisions of these general terms and conditions.</p>
+            <p>The review function on the platform may be used to facilitate buyer reviews on products. You shall not use the review function or any other form of communication to provide inaccurate, inauthentic or fake reviews.</p>
+            <p>We may periodically review your content and we reserve the right to remove any content in our discretion for any reason whatsoever.</p>
+
+            <h3>Our rights to use your content</h3>
+            <ul>
+              <li><p>You grant to us a worldwide, irrevocable, non-exclusive, royalty-free license to use, reproduce, store, adapt, publish, translate and distribute your content across our marketing channels and any existing or future media.</p></li>
+              <li><p>Without prejudice to our other rights under these general terms and conditions, if you breach our rules on content in any way, or if we reasonably suspect that you have breached our rules on content, we may delete, unpublish or edit any or all of your content.</p></li>
+            </ul>
+
+            <h3>Use of website and mobile applications</h3>
+            <h6>You may do the following in our applications:</h6>
+            <ul>
+              <li><p>View pages from our web application in a web browser;</p></li>
+              <li><p>Print pages from our website for your own personal and non commercial use, providing that such printing is not systematic or excessive;</p></li>
+              <li><p>Use our platform services by means of a web browser, subject to the other provisions of these general terms and conditions.</p></li><li><p></p></li><li><p></p></li><li><p></p></li>
+            </ul>
+            <p>You may only use our website for your own personal and business purposes in respect of selling, purchasing or delivering products on the platform.</p>
+            <p>We reserve the right to suspend or restrict access to our website, to areas of our website and/or to functionality upon our website. We may, for example, suspend access to the website during server maintenance or when we update the website. You must not attempt to circumvent or bypass, any access restriction measures on the website.</p>
+
+            <h6>You must not:</h6>
+            <ul>
+              <li><p>Hack or tamper with our website;</p></li>
+              <li><p>Circumvent any authentication or security systems or processes relating to our website</p></li>
+              <li><p>Do anything that interferes with the normal use of our website.</p></li>
+            </ul>
+
+            <h3>Copyright and trademarks</h3>
+            <p>Way4Biz’s logos and our other registered and unregistered trademarks are trademarks belonging to us; we give no permission for the use of these trademarks, and such use may constitute an infringement of our rights.</p>
+
+            <h3>Data Privacy</h3>
+            <p>Buyers agree to processing of their personal data in accordance with the terms of Way4Biz.</p>
+            <p>Way4Biz shall process all personal data obtained through the platform and related services accordingly.</p>
+
+            <h3>Way4Biz’s role as a platform</h3>
+            <h6>You acknowledge that:</h6>
+            <ul>
+              <li><p>We do confirm the identity of all platform users, check their credit worthiness, or otherwise vet them;</p></li>
+              <li><p>We do check, audit and monitor all information contained in catalogues;</p></li>
+              <li><p>We are not the agents for any buyer or seller, and accordingly we will not be liable to any person in relation to the offer for sale, sale or purchase of any products advertised on our platform; furthermore we are not responsible for the enforcement of any contractual obligations arising out of a contract for the sale or purchase of any products and we will have no obligation to mediate between the parties to any such contract.</p></li>
+            </ul>
+
+            <h6>We do not warrant that:</h6>
+            <ul>
+              <li><p>That the platform will operate without fault;</p></li>
+              <li><p>That the platform or any service on the platform will remain available.</p></li>
+              <li><p>that the material on the platform is up to date;</p></li>
+            </ul>
+
+            <p>We do not guarantee any commercial results concerning the use of the platform.</p>
+            <p>We reserve the right to discontinue or alter any or all of our platform services, and to stop publishing our platform, at any time in our sole discretion without notice or explanation; and you will not be entitled to any compensation or other payment upon the discontinuance or alteration of any platform services, or if we stop publishing the platform.</p>
+          </div>
         </div>
         <div>
           <div className="checkbox d-flex align-items-center ml-2 my-2">
