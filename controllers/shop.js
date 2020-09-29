322f3acb00322e483dbdc202dcba62b738ed20d8
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-29 17:06:49 +0300

    removed /api/checkout

--- controllers/shop.js
+++ controllers/shop.js
@@ -266,68 +266,6 @@ route.get("/api/orders", auth, async (req, res) => {
   }
 });
 
-route.get("/api/checkout", auth, (req, res) => {
-  try {
-    const { firstName, lastName, number } = req.session.user;
-    if (!number) {
-      return res.send({ firstName, lastName });
-    }
-    res.send({ firstName, lastName, number });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-route.post(
-  "/api/checkout",
-  auth,
-  check("firstName")
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage("Please enter your name with a minimum of three characters"),
-  check("lastName")
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage("Please enter your name with a minimum of three characters"),
-  check("number")
-    .isNumeric()
-    .isLength({ min: 12 })
-    .withMessage("Invalid Number"),
-  check("deliveryAddress")
-    .trim()
-    .isLength({ min: 2 })
-    .withMessage("Please enter a valid delivery address"),
-  check("city").trim().isLength({ min: 2 }).withMessage("Choose a valid city"),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(401).send({ message: errors.array()[0].msg });
-      }
-      const {
-        firstName,
-        lastName,
-        number,
-        deliveryAddress,
-        region,
-        city
-      } = req.body;
-      // CALCULATE DISTANCE AND DISTANCE CHARGES BASED ON REGION/CITY AND INCLUDE IT IN THE REQ.DELIVERY
-      req.delivery = {
-        firstName,
-        lastName,
-        number,
-        deliveryAddress,
-        region,
-        city
-      };
-      res.send({ price: 50000 });
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  }
-);
-
 let checkoutRequestId;
 let orderId;
 route.post(
