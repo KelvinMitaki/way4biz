0c94783b6e732569874ffc864c284ae9c278c8e4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-30 00:38:41 +0300

    payment button clicked once

--- client/src/components/CardPayment/CardPaymentButton.js
+++ client/src/components/CardPayment/CardPaymentButton.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { saveOrder } from "../../redux/actions";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import "./CardPaymentButton.css";
-import { useRef } from "react";
 
 const CardPaymentButton = ({ saveOrder, history }) => {
+  const [clicked, setClicked] = useState(false);
   const payment = useRef();
   const makePayment = () => {
-    saveOrder(history);
+    setClicked(true);
+    if (!clicked) {
+      saveOrder(history);
+    }
   };
   return (
     <div>
       <button
+        disabled={clicked}
         ref={payment}
         onClick={makePayment}
         className="secondary-button card-pay-button btn btn-block"
