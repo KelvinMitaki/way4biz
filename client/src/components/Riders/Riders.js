fa684e48b6e4b524ee9d6261c8f071548d7b8ac6
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-14 20:19:38 +0300

    Added logo to rider header

--- client/src/components/Riders/Riders.js
+++ client/src/components/Riders/Riders.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import "./Riders.css";
+import Logo from "../Header/Logo";
 
 class Riders extends React.Component {
   render() {
     return (
       <div>
-        <div id="rider-header"></div>
+        <div id="rider-header">
+          <Logo />
+        </div>
       </div>
     );
   }
