6147f0f310b7dd5c73ba397172d3775958ccd404
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-15 22:10:16 +0300

    Added pending deliveries component

---
+++ client/src/components/Riders/PendingDeliveries.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./PendingDeliveries.css";
+
+class PendingDeliveries extends React.Component {
+  render() {
+    return (
+      <div className="pending-deliveries">
+        <h3>Helloo World</h3>
+      </div>
+    );
+  }
+}
+
+export default PendingDeliveries;
