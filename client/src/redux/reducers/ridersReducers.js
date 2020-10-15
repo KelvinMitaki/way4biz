e81dec093ce439fe9f9c0b4b6b05cdd1f2f605c2
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-15 23:02:31 +0300

    Added deal fetch reducer

---
+++ client/src/redux/reducers/ridersReducers.js
@@ -0,0 +1,27 @@
+import {
+  FETCH_SUCCESSFUL_DELIVERIES_START,
+  SUCCESSFUL_DELIVERIES_FETCHED,
+  FETCH_SUCCESSFUL_DELIVERIES_STOP,
+} from "../actions/types";
+
+const initialState = {
+  fetchingSuccessfulDeliveries: false,
+  successfulDeliveriesFetched: false,
+};
+
+export const ridersReducer = (state = initialsState, action) => {
+  switch (action.type) {
+    case FETCH_SUCCESSFUL_DELIVERIES_START:
+      return { ...state, fetchingSuccessfulDeliveries: true };
+    case SUCCESSFUL_DELIVERIES_FETCHED:
+      return {
+        ...state,
+        fetchingSuccessfulDeliveries: false,
+        successfulDeliveriesFetched: true,
+      };
+    case FETCH_SUCCESSFUL_DELIVERIES_STOP:
+      return { ...state, fetchingSuccessfulDeliveries: false };
+    default:
+      return state;
+  }
+};
