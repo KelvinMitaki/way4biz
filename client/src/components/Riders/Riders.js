e83221583cb6e22cda8c3791b9d225b7c4b492d5
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-15 21:15:38 +0300

    Added riders tabs

--- client/src/components/Riders/Riders.js
+++ client/src/components/Riders/Riders.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Tabs from "react-responsive-tabs";
 
 import "./Riders.css";
 import Logo from "../Header/Logo";
 
 class Riders extends React.Component {
+  getTabs() {
+    const data = [
+      {
+        name: "SUccessful Deliveries",
+        content: "Helloo",
+      },
+      {
+        name: "Pending Deliveries",
+        content: "World",
+      },
+    ];
+
+    return data.map((d, index) => ({
+      title: d.name,
+      getContent: () => d.content,
+      key: index,
+      tabClassName: "rider-tab",
+      panelClassName: "seller-db-panel",
+    }));
+  }
   render() {
     return (
       <div>
@@ -15,9 +36,17 @@ class Riders extends React.Component {
         <div className="white-body rider-body">
           <div className="container">
             <div>
-              <h3>Welcome Driver</h3>
+              <h3>Welcome Rider,</h3>
             </div>
-            <div></div>
+            <div>
+              <Tabs
+                items={this.getTabs()}
+                transformWidth={100}
+                transform={true}
+                showMoreLabel={"More..."}
+                showInkBar={true}
+              />
+            </div>
           </div>
         </div>
       </div>
