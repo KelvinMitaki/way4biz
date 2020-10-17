41acb9f24fca695173aaab60b06f700a57117552
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-17 14:21:07 +0300

    Fixed admin driver issue

--- client/src/components/Admin/AdminDriver.js
+++ client/src/components/Admin/AdminDriver.js
@@ -10,32 +10,34 @@ import MobileLogo from "../Header/MobileLogo";
 
 class AdminDriver extends React.Component {
   render() {
-    <div className="container-fluid p-0 mb-5">
-      <MobileLogo />
-      <DashBoardHeader />
-      <SecondaryHeader />
-      <div className="mt-4 container">
-        <div className="box-container">
-          <div className="d-flex align-items-center">
-            <div style={{ flex: "1" }}>
-              <IconContext.Provider
-                value={{ className: "arrow-icon ml-3 my-2" }}
-              >
-                <div className="d-flex align-items-center">
-                  <Link to="/admin/divers">
-                    <BsArrowLeft />
-                  </Link>
-                </div>
-              </IconContext.Provider>
-            </div>
+    return (
+      <div className="container-fluid p-0 mb-5">
+        <MobileLogo />
+        <DashBoardHeader />
+        <SecondaryHeader />
+        <div className="mt-4 container">
+          <div className="box-container">
+            <div className="d-flex align-items-center">
+              <div style={{ flex: "1" }}>
+                <IconContext.Provider
+                  value={{ className: "arrow-icon ml-3 my-2" }}
+                >
+                  <div className="d-flex align-items-center">
+                    <Link to="/admin/divers">
+                      <BsArrowLeft />
+                    </Link>
+                  </div>
+                </IconContext.Provider>
+              </div>
 
-            <h3 className="ml-1" style={{ flex: "2" }}>
-              Driver Details
-            </h3>
+              <h3 className="ml-1" style={{ flex: "2" }}>
+                Driver Details
+              </h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>;
+    );
   }
 }
 const mapStateToProps = (state) => {
