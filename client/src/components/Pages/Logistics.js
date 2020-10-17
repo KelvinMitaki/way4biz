d664fe8ae7c400784c980b0b5e502d4a34a07364
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-17 13:33:29 +0300

    Changed logistics term

--- client/src/components/Pages/Logistics.js
+++ client/src/components/Pages/Logistics.js
@@ -22,8 +22,8 @@ class Logistics extends React.Component {
     townLatLng: {},
     addressLatLng: {
       lat: -1.28585,
-      lng: 36.8263
-    }
+      lng: 36.8263,
+    },
   };
   componentDidMount() {
     if (this.props.initialValues.city) {
@@ -39,26 +39,26 @@ class Logistics extends React.Component {
       this.handleAddressSelect(address);
     }
   }
-  handleCitySelect = async selectedCity => {
+  handleCitySelect = async (selectedCity) => {
     const results = await geocodeByAddress(selectedCity);
     const latlng = await getLatLng(results[0]);
     this.setState({ cityLatLng: latlng });
     this.props.change("city", selectedCity);
   };
-  handleTownSelect = async selectedTown => {
+  handleTownSelect = async (selectedTown) => {
     const results = await geocodeByAddress(selectedTown);
     const latlng = await getLatLng(results[0]);
     this.setState({ townLatLng: latlng });
     this.props.change("town", selectedTown);
   };
-  handleAddressSelect = async selectedAddress => {
+  handleAddressSelect = async (selectedAddress) => {
     const results = await geocodeByAddress(selectedAddress);
     const latlng = await getLatLng(results[0]);
     this.setState({ addressLatLng: latlng });
     this.props.change("address", selectedAddress);
   };
 
-  handleToAddressSelect = async selectedAddress => {
+  handleToAddressSelect = async (selectedAddress) => {
     const results = await geocodeByAddress(selectedAddress);
     const latlng = await getLatLng(results[0]);
     this.setState({ addressLatLng: latlng });
@@ -79,13 +79,13 @@ class Logistics extends React.Component {
               >
                 <h3 className="legend text-center">Logistics</h3>
                 <p className="my-2">
-                  Our logistics team will collect your goods from your current
-                  location to your desired location. Fill the form below and
-                  click request service{" "}
-                  <b>Note: The service is operational only within Nairobi.</b>.
+                  Our logistics personnel will collect your goods from your
+                  current location to your desired location. Fill the form below
+                  and click request service.{" "}
+                  <b>Note: The service is operational only within Nairobi.</b>
                 </p>
                 <form
-                  onSubmit={this.props.handleSubmit(formValues => {
+                  onSubmit={this.props.handleSubmit((formValues) => {
                     // this.props.storeLatLng(
                     //   `${this.state.addressLatLng.lat.toString()},${this.state.addressLatLng.lng.toString()}`
                     // );
@@ -138,7 +138,7 @@ class Logistics extends React.Component {
                     options={{
                       location: new google.maps.LatLng(this.state.cityLatLng),
                       radius: 1000,
-                      types: ["establishment"]
+                      types: ["establishment"],
                     }}
                     onSelect={this.handleToAddressSelect}
                   />
@@ -188,7 +188,7 @@ class Logistics extends React.Component {
                     options={{
                       location: new google.maps.LatLng(this.state.cityLatLng),
                       radius: 1000,
-                      types: ["establishment"]
+                      types: ["establishment"],
                     }}
                     onSelect={this.handleAddressSelect}
                   />
@@ -243,7 +243,7 @@ class Logistics extends React.Component {
     );
   }
 }
-const validate = formValues => {
+const validate = (formValues) => {
   const errors = {};
   if (
     !formValues.firstName ||
@@ -283,12 +283,12 @@ const validate = formValues => {
   }
   return errors;
 };
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     initialValues: state.auth.user,
     checkoutUserLoading: state.auth.checkoutUserLoading,
     checkoutUserError: state.auth.checkoutUserError,
-    cart: state.cartReducer.cart
+    cart: state.cartReducer.cart,
   };
 };
 export default withRouter(
