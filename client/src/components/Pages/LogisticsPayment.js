1121da9ab1eb9a4a6d652ff45724e810fd7fc609
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-18 16:45:26 +0300

    Added logistics payment confirm page

---
+++ client/src/components/Pages/LogisticsPayment.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import MiniMenuWrapper from "../MiniMenuWrapper/MiniMenuWrapper";
+import { connect } from "react-redux";
+import "./LogisticsPayment.css";
+import MobileLogo from "../Header/MobileLogo";
+
+class LogisticsPayment extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    return (
+      <div className="main">
+        <div className="content">
+          <MobileLogo />
+          <Header />
+        </div>
+        <Footer />
+        <MiniMenuWrapper />
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {};
+};
+export default connect(mapStateToProps)(LogisticsPayment);
