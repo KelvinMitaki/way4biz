cf998ce04d783d01fd6ce30c9084b51d4e031c36
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-20 19:44:32 +0300

    Modified account logistics content widths

--- client/src/components/Account/AccountLogistics.js
+++ client/src/components/Account/AccountLogistics.js
@@ -23,7 +23,7 @@ class AccountLogistics extends React.Component {
               </div>
               <div className="col-lg-8  box-container">
                 {/* deliveries */}
-                {/* <React.Fragment>
+                <React.Fragment>
                   {" "}
                   <div className="container mb-3">
                     <h3 className="mt-2" style={{ textAlign: "center" }}>
@@ -32,26 +32,34 @@ class AccountLogistics extends React.Component {
                   </div>
                   <div className="container y">
                     <div className="row">
-                      <div className="col-md-5">
+                      <div className="col-md-3">
+                        <h6>Item</h6>
+                      </div>
+                      <div className="col-md-3">
                         <h6>Delivered From</h6>
                       </div>
-                      <div className="col-md-4">
+                      <div className="col-md-3">
                         <h6>Delivered To</h6>
                       </div>
                       <div className="col-md-3"></div>
                     </div>
                   </div>
                   <div className="container">
-                    
-
+                    {/* mapping here */}
                     <div className="row box-container account-complaint-wrapper">
-                      <div className="col-md-5">
+                      <div className="col-md-3">
+                        <p>
+                          <strong className="mr-2 x">Item: </strong>
+                          <span>Pizza</span>
+                        </p>
+                      </div>
+                      <div className="col-md-3">
                         <p>
                           <strong className="mr-2 x">Delivered From: </strong>
                           <span>Zimmerman</span>
                         </p>
                       </div>
-                      <div className="col-md-4">
+                      <div className="col-md-3">
                         <p>
                           <strong className="mr-2 x">Delivered To:</strong>
                           <span>Nairobi CBD</span>
@@ -69,9 +77,9 @@ class AccountLogistics extends React.Component {
                       </div>
                     </div>
                   </div>
-                </React.Fragment> */}
+                </React.Fragment>
                 {/* No deliveries */}
-                <div className="no-account-deliveries">
+                {/* <div className="no-account-deliveries">
                   <RiMotorbikeLine
                     style={{ fontSize: "100px", color: "#f76b1a" }}
                   />
@@ -86,7 +94,7 @@ class AccountLogistics extends React.Component {
                   >
                     Get Started
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
