4f277cb17e23e550d999779e4df0de1c3b7e6d58
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-20 19:40:43 +0300

    Added account logistics content style

--- client/src/components/Account/AccountLogistics.js
+++ client/src/components/Account/AccountLogistics.js
@@ -23,7 +23,7 @@ class AccountLogistics extends React.Component {
               </div>
               <div className="col-lg-8  box-container">
                 {/* deliveries */}
-                <React.Fragment>
+                {/* <React.Fragment>
                   {" "}
                   <div className="container mb-3">
                     <h3 className="mt-2" style={{ textAlign: "center" }}>
@@ -42,7 +42,7 @@ class AccountLogistics extends React.Component {
                     </div>
                   </div>
                   <div className="container">
-                    {/* mapping here */}
+                    
 
                     <div className="row box-container account-complaint-wrapper">
                       <div className="col-md-5">
@@ -69,18 +69,21 @@ class AccountLogistics extends React.Component {
                       </div>
                     </div>
                   </div>
-                </React.Fragment>
+                </React.Fragment> */}
                 {/* No deliveries */}
-                <div className="no-buyer-complaints">
+                <div className="no-account-deliveries">
                   <RiMotorbikeLine
                     style={{ fontSize: "100px", color: "#f76b1a" }}
                   />
-                  <h5 className="mt-3">
+                  <p className="mt-3">
                     You haven't requested any delivery service yet.You have an
                     item to be delivered from point A to point B ? Click the Get
                     Started button.
-                  </h5>
-                  <Link to="/logistics" className="secondary-button">
+                  </p>
+                  <Link
+                    to="/logistics"
+                    className="btn d-flex align-items-center justify-content-center secondary-button"
+                  >
                     Get Started
                   </Link>
                 </div>
