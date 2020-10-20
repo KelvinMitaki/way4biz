ab77a2cdd3ed69f8427d6a9e142cbe784b50dc8a
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-20 12:15:23 +0300

    Added template

--- client/src/components/Pages/LogisticsPayment.js
+++ client/src/components/Pages/LogisticsPayment.js
@@ -9,6 +9,7 @@ import { Link, Redirect } from "react-router-dom";
 import { fetchDelivery } from "../../redux/actions";
 import ScreenLoader from "./ScreenLoader";
 import NotFound from "./NotFound";
+import { BsCheckCircle } from "react-icons/bs";
 
 class LogisticsPayment extends React.Component {
   componentDidMount() {
@@ -36,59 +37,72 @@ class LogisticsPayment extends React.Component {
           <Header />
           <div className="container-fluid white-body logistics-payment-wrapper">
             {/* show this before payment */}
-            <div className="container text-center pt-5 logisitics-confirm-details">
+            {/* <div className="col-md-7 col-lg-5 mx-auto text-left pt-5">
               <h4 className="mb-3">You are almost done!</h4>
-              <p>
+              <p className="mb-1">
                 The service will cost you ksh.{" "}
                 {Math.round(this.props.fetchedDelivery.charge).toLocaleString()}
                 . You will be required to give the payment to the delivery guy
                 together with the item to deliver.
               </p>
-              <h5>Details</h5>
-              <p>
-                <b>Sender: </b>Patrick Thumbi
-              </p>
-              <p>
-                <b>Phone: </b>079900000
-              </p>
-              <p>
-                <b>Item Name: </b>Pizza
-              </p>
-              <p>
-                <b>Item Qty: </b>1 Piece
-              </p>
-              <p>
-                <b>From: </b>Rongai Tuskys
-              </p>
-              <p>
-                <b>To: </b>Thika Road Mall
-              </p>
-              <p>
-                <b>Recipient: </b>Stacy Mwangi
-              </p>
-              <p>
-                <b>Phone: </b>079900000
-              </p>
 
-              <Link to="/logistics">Edit</Link>
-              <p>
+              <div className="ml-3 text-left logisitics-confirm-details">
+                <h5>Details</h5>
+                <p>
+                  <b>Sender: </b>Patrick Thumbi
+                </p>
+                <p>
+                  <b>Phone: </b>079900000
+                </p>
+                <p>
+                  <b>Item Name: </b>Pizza
+                </p>
+                <p>
+                  <b>Item Qty: </b>1 Piece
+                </p>
+                <p>
+                  <b>From: </b>Rongai Tuskys
+                </p>
+                <p>
+                  <b>To: </b>Thika Road Mall
+                </p>
+                <p>
+                  <b>Recipient: </b>Stacy Mwangi
+                </p>
+                <p>
+                  <b>Phone: </b>079900000
+                </p>
+
+                <Link to="/logistics">Edit</Link>
+              </div>
+
+              <p className="mt-1">
                 Please confirm payment and we will have someone come pick your
                 item for delivery.
               </p>
-              <button className="btn btn-md mt-3 secondary-button logistics-confirm-payment">
+              <button className="btn btn-md my-3 secondary-button logistics-confirm-payment">
                 Confirm Payment
               </button>
-            </div>
+            </div> */}
             {/* show this after payment */}
-            {/* <div className="container text-center pt-5">
+            <div className="col-md-7 col-lg-5 mx-auto text-center pt-5">
               <div className="d-flex align-items-center justify-content-center">
                 <BsCheckCircle
                   style={{ fontSize: "100px", color: "#4BB543" }}
                 />
               </div>
               <h4 className="mb-3">The driver is on the way!</h4>
+              <div className="ml-2 logistics-driver-details">
+                <h5>Driver Details</h5>
+                <p>
+                  <b>Driver Phone: </b>0799000000
+                </p>
+                <p>
+                  <b>Vehicle No.: </b>KMCM 413D
+                </p>
+              </div>
               <p>You will be notified when he arrives.</p>
-            </div> */}
+            </div>
           </div>
         </div>
         <Footer />
